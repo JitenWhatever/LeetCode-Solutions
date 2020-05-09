14eacfd0613e305c44b66f3fe8f72de4af56a31b
Jitendra <jitendra1551k@gmail.com>
2020-05-10 00:29:00 +0530

    handle overflow is tricky

---
+++ Easy/Reverse Integer.java
@@ -0,0 +1,36 @@
+/*
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+Input: 123
+Output: 321
+
+Example 2:
+Input: -123
+Output: -321
+
+Example 3:
+Input: 120
+Output: 21
+
+Note:
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+*/
+
+class Solution {
+    public int reverse(int x) {
+        int reverseNumber  = 0;
+        while(x != 0){
+           
+            int digit  = x%10;
+            if (reverseNumber > Integer.MAX_VALUE/10 || (reverseNumber == Integer.MAX_VALUE / 10 && digit > 7)) return 0;
+            if (reverseNumber < Integer.MIN_VALUE/10 || (reverseNumber == Integer.MIN_VALUE / 10 && digit < -8)) return 0;
+            reverseNumber = reverseNumber*10 + (digit);
+            x /= 10; 
+            
+        }
+        
+        return reverseNumber;
+    }
+}
