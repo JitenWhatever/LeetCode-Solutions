a64df29a205007b08b62d1e3587b8844ddbadb76
Jitendra <jitendra1551k@gmail.com>
2020-05-29 03:14:33 +0530

    binary search and greedy

---
+++ Medium/Search in Rotated Sorted Array.java
@@ -0,0 +1,66 @@
+/*
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+*/
+
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        if(nums.length == 0) {
+            return -1;
+        }
+        int low  = 0, high = nums.length - 1;
+        while(low  < high) {
+            int mid = low + (high - low)/2;
+            if(nums[mid] > nums[high]) {
+                low = mid + 1;
+            }
+            else {
+                high = mid;
+            }
+        }
+        
+        int pivot = low;
+        low = 0;
+        high = nums.length - 1;
+        if(nums[pivot] <= target && target <= nums[high]) {
+            low = pivot;
+        }
+        else {
+            high = pivot;
+        }
+        
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            
+            if(nums[mid] == target) {
+                return mid;
+            }
+            else if(nums[mid] > target) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+}
