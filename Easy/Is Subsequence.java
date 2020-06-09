ad8b50e3ec491a62c698ac6494ef3db8c2d318ad
Jitendra <jitendra1551k@gmail.com>
2020-06-09 21:21:26 +0530

    bit manipulation and double pointer

---
+++ Easy/Is Subsequence.java
@@ -0,0 +1,55 @@
+/*
+Given a string s and a string t, check if s is subsequence of t.
+
+A subsequence of a string is a new string which is formed from the 
+original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. 
+(ie, "ace" is a subsequence of "abcde" while "aec" is not).
+
+Follow up:
+If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, 
+and you want to check one by one to see if T has its subsequence. 
+In this scenario, how would you change your code?
+
+Credits:
+Special thanks to @pbrother for adding this problem and creating all test cases.
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+Constraints:
+    0 <= s.length <= 100
+    0 <= t.length <= 10^4
+    Both strings consists only of lowercase characters.
+*/
+
+class Solution {
+    public boolean isSubsequence(String A, String B) {
+        int N = A.length();
+        int M = B.length();
+        
+        if(N > M) {
+            return false;
+        }
+        
+        int index_a = 0, index_b = 0;
+        
+        while(index_a < N && index_b < M) {
+            if(A.charAt(index_a) == B.charAt(index_b)) {
+                ++index_a;
+                ++index_b;
+            }
+            else {
+                ++index_b;
+            }
+        }
+        
+        return index_a == N;
+    }
+}
--- Easy/Power of Two.java
+++ Easy/Power of Two.java
@@ -2,36 +2,36 @@
 Given an integer, write a function to determine if it is a power of two.
 
 Example 1:
+
 Input: 1
 Output: true 
 Explanation: 20 = 1
 
 Example 2:
+
 Input: 16
 Output: true
 Explanation: 24 = 16
 
 Example 3:
+
 Input: 218
 Output: false
 */
 
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n < 1) return false;
-        int bitCount = 0;
-        for(int bit = 0; bit < 32; ++bit){
-            if((n % 2) == 1){
-                ++bitCount;
-            }
-            
-            n = n/2;
-            
-            if(bitCount > 1){
-                return false;
-            }
+        
+        if(n == Integer.MIN_VALUE || n == Integer.MAX_VALUE || n == 0) {
+            return false;
         }
         
-        return true;
+        int result = n&(n-1);
+        
+        if(result == 0) {
+            return true;
+        }
+        
+        return false;
     }
 }
