f12ba3cb46128a6f2c307bcbb11e1648d5a3bcf4
Jitendra <jitendra1551k@gmail.com>
2020-07-08 20:51:22 +0530

    sorting and two pointers

---
+++ Medium/3Sum.java
@@ -0,0 +1,57 @@
+/*
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? 
+Find all unique triplets in the array which gives the sum of zero.
+
+Note:
+
+The solution set must not contain duplicate triplets.
+
+Example:
+
+Given array nums = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+*/
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList();
+        
+        if(nums == null || nums.length < 3) {
+            return result;
+        }
+        Arrays.sort(nums);
+        for(int index = 0; index < nums.length - 2; ++index) {
+            
+            if(index == 0 || index > 0 && nums[index] != nums[index - 1]) {
+                int sum = -nums[index], low = index + 1, high = nums.length - 1;
+                while(low < high) {
+                    if(sum == nums[low] + nums[high]) {
+                        result.add(Arrays.asList(-sum, nums[low], nums[high]));
+                        while(low < high && nums[low] == nums[low + 1]) {
+                            ++low;
+                        }
+                        while(low < high && nums[high] == nums[high - 1]) {
+                            --high;
+                        }
+                        
+                        ++low;
+                        --high;
+                    }
+                    else if(sum < nums[low] + nums[high]) {
+                        --high;
+                    }
+                    else {
+                        ++low;
+                    }
+                }   
+            }
+        }
+        
+        return result;
+    }
+}
