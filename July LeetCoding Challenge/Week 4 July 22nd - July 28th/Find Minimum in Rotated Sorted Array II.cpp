949298c2a1ddd84999c407f6aeb1b2ce9d626d93
Jitendra <jitendra1551k@gmail.com>
2020-07-25 13:01:33 +0530

    Binary search

---
+++ July LeetCoding Challenge/Week 4 July 22nd - July 28th/Find Minimum in Rotated Sorted Array II.cpp
@@ -0,0 +1,48 @@
+/*
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+
+Find the minimum element.
+
+The array may contain duplicates.
+
+Example 1:
+
+Input: [1,3,5]
+Output: 1
+Example 2:
+
+Input: [2,2,2,0,1]
+Output: 0
+Note:
+
+This is a follow up problem to Find Minimum in Rotated Sorted Array.
+Would allow duplicates affect the run-time complexity? How and why?
+*/
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        if(nums.size() == 0) {
+            return 0;
+        }
+        int low = 0, high = nums.size() - 1;
+        
+        while(low < high) {
+            int mid = low + (high - low)/2;
+            
+            if(nums[mid] > nums[high]) {
+                low = mid + 1;
+            }
+            else if(nums[mid] < nums[low]) {
+                high = mid;
+            }
+            else {
+                --high;
+            }
+        }
+        
+        return nums[low];
+    }
+};
