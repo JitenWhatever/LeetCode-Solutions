3c97e8f936744872a6f47c3ce89ef8ee0ff6a8a3
Jitendra <jitendra1551k@gmail.com>
2020-08-15 01:05:49 +0530

    simple hash count

---
+++ August LeetCoding Challenge/Week 2 August 8th - August 14th/Longest Palindrome.cpp
@@ -0,0 +1,41 @@
+/*
+Given a string which consists of lowercase or uppercase letters, find the length of the longest palindromes that can be built with those letters.
+
+This is case sensitive, for example "Aa" is not considered a palindrome here.
+
+Note:
+Assume the length of given string will not exceed 1,010.
+
+Example:
+
+Input:
+"abccccdd"
+
+Output:
+7
+
+Explanation:
+One longest palindrome that can be built is "dccaccd", whose length is 7.
+*/
+
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        vector<int> hash(128, 0);
+        
+        for(char ch : s) {
+            ++hash[ch];    
+        }
+        
+        int result = 0;
+        for(int count : hash) {
+            result += (count / 2) * 2;
+            
+            if(result%2 == 0 && count % 2 == 1) {
+                ++result;
+            }
+        }
+        
+        return result;
+    }
+};
