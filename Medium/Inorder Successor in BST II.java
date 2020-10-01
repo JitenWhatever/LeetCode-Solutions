a060b0d84e3faae244a1b9a8afd99c30c2fc2078
Jitendra <jitendra1551k@gmail.com>
2020-10-01 23:03:06 +0530

    similar to first version

---
+++ Medium/Inorder Successor in BST II.java
@@ -0,0 +1,46 @@
+/*
+Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+
+The successor of a node p is the node with the smallest key greater than p.val. 
+You will have direct access to the node but not to the root of the tree. 
+Each node will have a reference to its parent node. 
+A node is defined as the following:
+*/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+};
+*/
+
+class Solution {
+    public Node inorderSuccessor(Node x) {
+    Node result = null;
+    
+    Node p = x.right;
+    while(p != null){ 
+        result = p;
+        p = p.left;
+    }
+    
+    if(result != null){
+        return result; 
+    }
+    
+    p = x;
+    
+    while(p != null){
+        if(p.parent != null && p.parent.left == p){
+        return p.parent;
+        } 
+        p = p.parent;
+    }
+    
+    return null;
+    }
+
+}
