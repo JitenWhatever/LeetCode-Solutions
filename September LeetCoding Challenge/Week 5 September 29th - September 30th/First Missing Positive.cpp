596af12aac664ec3cc59fdaa559e825c5a54e757
Jitendra <jitendra1551k@gmail.com>
2020-10-01 22:44:51 +0530

    number to index

---
+++ September LeetCoding Challenge/Week 5 September 29th - September 30th/First Missing Positive.cpp
@@ -0,0 +1,39 @@
+/*
+Given an unsorted integer array, find the smallest missing positive integer.
+
+Example 1:
+
+Input: [1,2,0]
+Output: 3
+Example 2:
+
+Input: [3,4,-1,1]
+Output: 2
+Example 3:
+
+Input: [7,8,9,11,12]
+Output: 1
+Follow up:
+
+Your algorithm should run in O(n) time and uses constant extra space.
+*/
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        set<int> st;
+        for(int num : nums) {
+            if(num > 0)
+                st.insert(num);
+        }
+        int index = 1;
+        for(int num : st) {
+            if(num != index) {
+                return index;
+            }
+            ++index;
+        }
+        
+        return index;
+    }
+};
