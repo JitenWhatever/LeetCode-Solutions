28f1e872014c959f409ac43cc34cb138d3cb80b7
Jitendra <jitendra1551k@gmail.com>
2020-10-17 11:14:03 +0530

    binary search, modulo, dp

---
+++ Medium/House Robber II.java
@@ -0,0 +1,57 @@
+/*
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. 
+All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. 
+Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+Example 2:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+Example 3:
+
+Input: nums = [0]
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 1000
+*/
+
+class Solution {
+    public int rob(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        int len = nums.length;
+        if(len == 1) {
+            return nums[0];
+        }
+        return Math.max(houseRob(0, len - 1, nums), houseRob(1, len, nums));
+    }
+    
+    private int houseRob(int l, int r, int[] nums) {
+        int last2 = 0, last1 = 0, money = 0;
+        
+        for(int house = l; house < r; ++house) {
+            money = Math.max(last2 + nums[house], last1);
+            
+            last2 = last1;
+            last1 = money;
+        }
+        
+        return money;
+    }
+}
