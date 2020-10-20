885f67f279f1c88bfb010710e854d2b8b83c14a2
Jitendra <jitendra1551k@gmail.com>
2020-10-20 22:32:55 +0530

    only four cases to handle

---
+++ October LeetCoding Challenge/Week 3 October 15th - October 21st/Minimum Domino Rotations For Equal Row.cpp
@@ -0,0 +1,61 @@
+/*
+In a row of dominoes, A[i] and B[i] represent the top and bottom halves of the ith domino.  (A domino is a tile with two numbers from 1 to 6 - one on each half of the tile.)
+
+We may rotate the ith domino, so that A[i] and B[i] swap values.
+
+Return the minimum number of rotations so that all the values in A are the same, or all the values in B are the same.
+
+If it cannot be done, return -1.
+
+ 
+
+Example 1:
+https://assets.leetcode.com/uploads/2019/03/08/domino.png
+
+Input: A = [2,1,2,4,2,2], B = [5,2,6,2,3,2]
+Output: 2
+Explanation: 
+The first figure represents the dominoes as given by A and B: before we do any rotations.
+If we rotate the second and fourth dominoes, we can make every value in the top row equal to 2, as indicated by the second figure.
+Example 2:
+
+Input: A = [3,5,1,2,3], B = [3,6,3,3,4]
+Output: -1
+Explanation: 
+In this case, it is not possible to rotate the dominoes to make one row of values equal.
+ 
+
+Constraints:
+
+2 <= A.length == B.length <= 2 * 104
+1 <= A[i], B[i] <= 6
+*/
+
+
+class Solution {
+private:
+    int minSwaps(const int target, const vector<int>& A, const vector<int>& B) {
+        int swaps = 0;
+        
+        for(int index = 0; index < A.size(); ++index) {
+            if(target != A[index] && target != B[index]) {
+                return INT_MAX;
+            }
+            else if(target != A[index]) {
+                ++swaps;
+            }
+        }
+        
+        return swaps;
+    }
+public:
+    int minDominoRotations(vector<int>& A, vector<int>& B) {
+        
+        int minimumSwaps = min(minSwaps(A[0], A, B), minSwaps(B[0], A, B));
+        
+        minimumSwaps = min(minimumSwaps, minSwaps(A[0], B, A));
+        minimumSwaps = min(minimumSwaps, minSwaps(B[0], B, A));
+        
+        return minimumSwaps == INT_MAX ? -1 : minimumSwaps;;
+    }
+};
