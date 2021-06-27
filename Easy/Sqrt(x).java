584d3ac3b4c5c80895942a7de2c699d4c9b5c001
jitenwhatever <jitendra1551k@gmail.com>
2021-06-27 06:05:22 +0530

    binary search

--- Easy/Sqrt(x).java
+++ Easy/Sqrt(x).java
@@ -20,28 +20,22 @@ Explanation: The square root of 8 is 2.82842..., and since
 class Solution {
     public int mySqrt(int x) {
         
-            
         if(x < 2) {
             return x;
         }
-        
-        int low = 1;
-        int high = x;
-        
-        while(low < high) {
-            int mid = low + (high - low)/2;
-            
-            if(mid <= x/mid && (mid + 1) > x/(mid + 1)) {
+        int low = 2, high = x/2;
+        while(low <= high) {
+            int mid =  low + (high - low)/2;
+            if(mid  > x/mid) {
+                high = mid - 1;
+            } else if (mid < x/mid){
+               low = mid + 1;
+            } else {
                 return mid;
             }
-            else if(mid > x/mid) {
-                high = mid;
-            }
-            else {
-                low = mid + 1;
-            }
+            
         }
         
-        return low;
+        return high;
     }
 }
