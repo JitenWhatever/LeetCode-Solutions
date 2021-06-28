d64b47c8b71e6895fa7694f862fe5686c58067b3
jitenwhatever <jitendra1551k@gmail.com>
2021-06-28 23:10:34 +0530

    D&C

--- Easy/Majority Element.java
+++ Easy/Majority Element.java
@@ -36,3 +36,46 @@ class Solution {
         
     }
 }
+
+// Divide And Conquer
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+        if(Objects.isNull(nums) || nums.length == 0) {
+            return -1;
+        }
+        
+        return divideAndConquer(nums, 0, nums.length - 1);
+    }
+    
+    private int divideAndConquer(int[] nums, int left, int right) {
+        
+        if(left == right) {
+            return nums[left];
+        }
+        
+        int mid = left + (right - left) / 2;
+        
+        int leftMajor = divideAndConquer(nums, left, mid);
+        int rightMajor = divideAndConquer(nums, mid + 1, right);
+        
+        int leftCount = getCount(nums, leftMajor, left, right);
+        int rightCount = getCount(nums, rightMajor, left, right);
+        
+        
+        return leftCount > rightCount ? leftMajor : rightMajor;
+    }
+    
+    private int getCount(int[] nums, int num, int left, int right) {
+        int count = 0;
+        
+        for(int index = left; index <= right; ++index) {
+            if(nums[index] == num) {
+                ++count;
+            }
+        }
+        
+        return count;
+    }
+}
