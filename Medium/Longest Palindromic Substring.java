490ed862e44722278a2fa91434d84f9eb1c40a69
jitenwhatever <jitendra1551k@gmail.com>
2021-07-08 01:34:01 +0530

    dp

--- Medium/Longest Palindromic Substring.java
+++ Medium/Longest Palindromic Substring.java
@@ -47,4 +47,50 @@ class Solution {
         
         return (high - low - 1);
     }
+
+    public String longestPalindrome(String s) {
+    int n = s.length();
+    int left = 0, right = 0;
+        
+    boolean[][] dp = new boolean[n][n];
+    // dp[i][j] indicates whether substring s starting at index i and ending at j is palindrome    
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = i; j < n; j++) {
+            dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
+                    
+            if (dp[i][j] && (j - i + 1 > res.length())) {
+                left = i;
+                right = j + 1;
+            }
+        }
+    }
+        
+    return s.substring(left, right);
+    }
 }
+
+public class Solution {
+private int lo, maxLen;
+
+public String longestPalindrome(String s) {
+	int len = s.length();
+	if (len < 2)
+		return s;
+	
+    for (int i = 0; i < len-1; i++) {
+     	extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
+     	extendPalindrome(s, i, i+1); //assume even length.
+    }
+    return s.substring(lo, lo + maxLen);
+}
+
+private void extendPalindrome(String s, int j, int k) {
+	while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+		j--;
+		k++;
+	}
+	if (maxLen < k - j - 1) {
+		lo = j + 1;
+		maxLen = k - j - 1;
+	}
+}}
