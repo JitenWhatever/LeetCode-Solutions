118279d4697584d5521da1fff146755b509140be
jitenwhatever <jitendra1551k@gmail.com>
2021-07-11 05:25:39 +0530

    4sum

--- Medium/4Sum.java
+++ Medium/4Sum.java
@@ -1,73 +1,71 @@
 /*
-Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? 
-Find all unique quadruplets in the array which gives the sum of target.
+Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
 
-Note:
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+nums[a] + nums[b] + nums[c] + nums[d] == target
+You may return the answer in any order.
 
-The solution set must not contain duplicate quadruplets.
+ 
 
-Example:
+Example 1:
 
-Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+Example 2:
 
-A solution set is:
-[
-  [-1,  0, 0, 1],
-  [-2, -1, 1, 2],
-  [-2,  0, 0, 2]
-]
+Input: nums = [2,2,2,2,2], target = 8
+Output: [[2,2,2,2]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 200
+-109 <= nums[i] <= 109
+-109 <= target <= 109
 */
-
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList();
+        List<List<Integer>> quadruplets = new ArrayList<>();
         
-        if(nums == null || nums.length < 4) {
-            return result;
+        if (Objects.isNull(nums) || nums.length < 4) {
+            return quadruplets;
         }
         
         Arrays.sort(nums);
-        // for(int num : nums) {
-        //      System.out.println(num);
-        // }
-       
-        for(int index_a = 0; index_a < nums.length - 3; ++index_a) {
-            
-            if(index_a > 0 && nums[index_a] == nums[index_a - 1]) {
-                continue;
-            }
-                
-            for(int index_b = index_a + 1; index_b < nums.length - 2; ++index_b) {
-                    
-                if(index_b > index_a + 1 && nums[index_b] == nums[index_b - 1]) {
-                    continue;
-                }
-                        
-                int low = index_b + 1, high = nums.length - 1;
-                        
-                while(low < high) {
-                    int sum = nums[index_a] + nums[index_b] + nums[low] + nums[high];
-                            
-                    if(sum == target) {
-                        result.add(Arrays.asList(nums[index_a], nums[index_b], nums[low], nums[high]));
-                            while(low < high && nums[low] == nums[low + 1]) {
-                                ++low;
+        
+        for (int index_i = 0; index_i < nums.length - 3; ++index_i) {
+            if(index_i == 0 || nums[index_i] != nums[index_i - 1]) {
+                for (int index_j = index_i + 1; index_j < nums.length - 2; ++index_j) {
+                    if (index_j == index_i + 1  || nums[index_j] != nums[index_j - 1]) {
+
+                        int left = index_j + 1, right = nums.length - 1;
+
+                        while (left < right) {
+                            if (target == nums[index_i] + nums[index_j] + nums[left] + nums[right]) {
+                                quadruplets.add(Arrays.asList(nums[index_i], nums[index_j], nums[left], nums[right]));
+
+                                while (left < right && nums[left] == nums[left + 1]) {
+                                    ++left;
+                                }
+
+                                while (left < right && nums[right] == nums[right - 1]) {
+                                    --right;
+                                }
+
+                                ++left;
+                                --right;
+                            } else if (target > nums[index_i] + nums[index_j] + nums[left] + nums[right]) {
+                                ++left;
+                            } else {
+                                --right;
                             }
-                            while(low < high && nums[high] == nums[high - 1]) {
-                                --high;
-                            }                                
-                            ++low;
-                            --high;
-                        }
-                        else if(sum < target) {
-                            ++low;
-                        }
-                        else {
-                            --high;
                         }
                     }
                 }
             }
-        return result;
+        }
+            
+        return quadruplets;
     }
 }
