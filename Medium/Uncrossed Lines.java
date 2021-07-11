bf29b9d173023d84999de7ac346765fdd0541be7
jitenwhatever <jitendra1551k@gmail.com>
2021-07-12 00:05:39 +0530

    dp + string

--- Medium/Uncrossed Lines.java
+++ Medium/Uncrossed Lines.java
@@ -1,35 +1,38 @@
 /*
-We write the integers of A and B (in the order they are given) on two separate horizontal lines.
+We write the integers of nums1 and nums2 (in the order they are given) on two separate horizontal lines.
 
-Now, we may draw connecting lines: a straight line connecting two numbers A[i] and B[j] such that:
-    A[i] == B[j];
-    The line we draw does not intersect any other connecting (non-horizontal) line.
+Now, we may draw connecting lines: a straight line connecting two numbers nums1[i] and nums2[j] such that:
 
+nums1[i] == nums2[j];
+The line we draw does not intersect any other connecting (non-horizontal) line.
 Note that a connecting lines cannot intersect even at the endpoints: each number can only belong to one connecting line.
 
 Return the maximum number of connecting lines we can draw in this way.
 
-Example 1:
+ 
 
-Input: A = [1,4,2], B = [1,2,4]
+Example 1:
+https://assets.leetcode.com/uploads/2019/04/26/142.png
+
+Input: nums1 = [1,4,2], nums2 = [1,2,4]
 Output: 2
 Explanation: We can draw 2 uncrossed lines as in the diagram.
-We cannot draw 3 uncrossed lines, because the line from A[1]=4 to B[2]=4 will intersect the line from A[2]=2 to B[1]=2.
-
+We cannot draw 3 uncrossed lines, because the line from nums1[1]=4 to nums2[2]=4 will intersect the line from nums1[2]=2 to nums2[1]=2.
 Example 2:
 
-Input: A = [2,5,1,2,5], B = [10,5,2,1,5,2]
+Input: nums1 = [2,5,1,2,5], nums2 = [10,5,2,1,5,2]
 Output: 3
-
 Example 3:
 
-Input: A = [1,3,7,1,7,5], B = [1,9,2,5,1]
+Input: nums1 = [1,3,7,1,7,5], nums2 = [1,9,2,5,1]
 Output: 2
+ 
 
 Note:
-    1 <= A.length <= 500
-    1 <= B.length <= 500
-    1 <= A[i], B[i] <= 2000
+
+1 <= nums1.length <= 500
+1 <= nums2.length <= 500
+1 <= nums1[i], nums2[i] <= 2000
 */
 
 class Solution {
@@ -50,5 +53,39 @@ class Solution {
         }
         
         return dp[N][M];
+    }
+}
+
+class Solution {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+     
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        
+        dp = new Integer[nums1.length + 1][nums2.length + 1];
+        
+        recurse(nums1.length, nums2.length);
+        
+        return dp[nums1.length][nums2.length];
+    }
+    
+    private int[] nums1, nums2;
+    private Integer[][] dp;
+     
+    private int recurse(int index_i, int index_j) {
+        if (index_i == 0 || index_j == 0) {
+            return dp[index_i][index_j] = 0;
+        }
+        
+        if (Objects.nonNull(dp[index_i][index_j])) {
+            return dp[index_i][index_j];
+        }
+        
+        if (this.nums1[index_i - 1] == nums2[index_j - 1]) {
+            return dp[index_i][index_j] = 1 + recurse(index_i - 1, index_j - 1);
+        }
+        
+        
+        return dp[index_i][index_j] = Math.max(recurse(index_i, index_j - 1), recurse(index_i - 1, index_j));
     }
 }
