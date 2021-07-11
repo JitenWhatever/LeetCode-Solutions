4ef8a4a7dccbacbd687849ab676f14957373933a
jitenwhatever <jitendra1551k@gmail.com>
2021-07-12 00:25:40 +0530

    dp + string

--- Medium/Delete Operation for Two Strings.java
+++ Medium/Delete Operation for Two Strings.java
@@ -22,3 +22,44 @@ Constraints:
 word1 and word2 consist of only lowercase English letters.
 */
 
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        
+        for (int index_i = 1; index_i <= word1.length(); ++index_i) {
+            for (int index_j = 1; index_j <= word2.length(); ++index_j) {        
+                if (word1.charAt(index_i - 1) == word2.charAt(index_j - 1)) {
+                    dp[index_i][index_j] = 1 + dp[index_i - 1][index_j - 1];
+                } else {
+                    dp[index_i][index_j] = Math.max(dp[index_i - 1][index_j], dp[index_i][index_j - 1]);
+                }
+            }
+        }
+        
+        return word1.length() + word2.length() - 2*dp[word1.length()][word2.length()];
+    }
+}
+
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        
+        for (int index_i = 0; index_i <= word1.length(); ++index_i) {
+            for (int index_j = 0; index_j <= word2.length(); ++index_j) { 
+                
+                if (index_i == 0 || index_j == 0) {
+                    dp[index_i][index_j] = index_i + index_j;
+                    continue;
+                }
+                
+                if (word1.charAt(index_i - 1) == word2.charAt(index_j - 1)) {
+                    dp[index_i][index_j] = dp[index_i - 1][index_j - 1];
+                } else {
+                    dp[index_i][index_j] = Math.min(dp[index_i - 1][index_j], dp[index_i][index_j - 1]) + 1;
+                }
+            }
+        }
+        
+        return dp[word1.length()][word2.length()];
+    }
+}
