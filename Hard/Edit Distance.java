189ddac3e7d754ad85b4555ea8263a621b8fff31
jitenwhatever <jitendra1551k@gmail.com>
2021-07-11 23:06:29 +0530

    dp + string

--- Hard/Edit Distance.java
+++ Hard/Edit Distance.java
@@ -1,11 +1,12 @@
 /*
-Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
 
-You have the following 3 operations permitted on a word:
+You have the following three operations permitted on a word:
 
-    Insert a character
-    Delete a character
-    Replace a character
+Insert a character
+Delete a character
+Replace a character
+ 
 
 Example 1:
 
@@ -15,7 +16,6 @@ Explanation:
 horse -> rorse (replace 'h' with 'r')
 rorse -> rose (remove 'r')
 rose -> ros (remove 'e')
-
 Example 2:
 
 Input: word1 = "intention", word2 = "execution"
@@ -26,6 +26,12 @@ inention -> enention (replace 'i' with 'e')
 enention -> exention (replace 'n' with 'x')
 exention -> exection (replace 'n' with 'c')
 exection -> execution (insert 'u')
+ 
+
+Constraints:
+
+0 <= word1.length, word2.length <= 500
+word1 and word2 consist of lowercase English letters.
 */
 
 class Solution {
@@ -62,5 +68,40 @@ class Solution {
         }
         
         return dp[N][M];
+    }
+}
+
+
+class Solution {
+    public int minDistance(String word1, String word2) {
+        dp = new Integer[word1.length() + 1][word2.length() + 1];
+        recurse(word1, word2, 0, 0);
+        
+        return dp[0][0];
+    }
+    
+    private Integer[][] dp;
+    
+    private int recurse(String word1, String word2, int index_a, int index_b) {
+        if (word1.length() == index_a) {
+            return dp[index_a][index_b] = word2.length() - index_b;
+        } 
+        
+         if (word2.length() == index_b) {
+            return dp[index_a][index_b] = word1.length() - index_a;
+        }
+        
+        if (Objects.nonNull(dp[index_a][index_b])) {
+            return dp[index_a][index_b];
+        }
+        
+        if (word1.charAt(index_a) == word2.charAt(index_b)) {
+            return dp[index_a][index_b] = recurse(word1, word2, index_a + 1, index_b + 1);
+        }
+        
+        
+        return dp[index_a][index_b] = (Math.min(recurse(word1, word2, index_a + 1, index_b + 1), 
+                                              Math.min(recurse(word1, word2, index_a + 1, index_b), recurse(word1, word2, index_a, index_b + 1))) + 1);
+        
     }
 }
