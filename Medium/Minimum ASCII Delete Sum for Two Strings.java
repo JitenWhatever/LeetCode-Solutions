8e4750d87b6434889499c8f64c154b77f92ab7b0
jitenwhatever <jitendra1551k@gmail.com>
2021-07-12 23:09:05 +0530

    dp + string

--- Medium/Minimum ASCII Delete Sum for Two Strings.java
+++ Medium/Minimum ASCII Delete Sum for Two Strings.java
@@ -1,80 +1,101 @@
 /*
-Given two strings s1, s2, find the lowest ASCII sum of deleted characters to make two strings equal.
+Given two strings s1 and s2, return the lowest ASCII sum of deleted characters to make two strings equal.
 
 Example 1:
+
 Input: s1 = "sea", s2 = "eat"
 Output: 231
 Explanation: Deleting "s" from "sea" adds the ASCII value of "s" (115) to the sum.
 Deleting "t" from "eat" adds 116 to the sum.
 At the end, both strings are equal, and 115 + 116 = 231 is the minimum sum possible to achieve this.
 Example 2:
+
 Input: s1 = "delete", s2 = "leet"
 Output: 403
 Explanation: Deleting "dee" from "delete" to turn the string into "let",
-adds 100[d]+101[e]+101[e] to the sum.  Deleting "e" from "leet" adds 101[e] to the sum.
+adds 100[d] + 101[e] + 101[e] to the sum.
+Deleting "e" from "leet" adds 101[e] to the sum.
 At the end, both strings are equal to "let", and the answer is 100+101+101+101 = 403.
 If instead we turned both strings into "lee" or "eet", we would get answers of 433 or 417, which are higher.
-Note:
+ 
 
-0 < s1.length, s2.length <= 1000.
-All elements of each string will have an ASCII value in [97, 122].
+Constraints:
+
+1 <= s1.length, s2.length <= 1000
+s1 and s2 consist of lowercase English letters.
 */
 
 class Solution {
-    public int minimumDeleteSum(String s1, String s2) {
-        dp = new int[s1.length() + 1][s2.length() + 1];
-            
-        // return recurse(s1, s2, 0, 0);      // 30 ms
+    public int minimumDeleteSum(String s1, String s2) {        
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
         
-        for(int j = 1; j <= s2.length(); ++j) {
-            dp[0][j] = dp[0][j - 1] + (int)s2.charAt(j - 1);
-        }
-        
-        for(int i = 1; i <= s1.length(); ++i) {
-            dp[i][0] = dp[i - 1][0] + (int)s1.charAt(i - 1);
-        }
-        
-        for(int i = 1; i <= s1.length(); ++i) {
-            for(int j = 1; j <= s2.length(); ++j) {
-                if(s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
+        for (int index_i = 0; index_i <= s1.length(); ++index_i) {
+            for (int index_j = 0; index_j <= s2.length(); ++index_j) {
+                if (index_i == 0 && index_j == 0) {
+                    dp[index_i][index_j] = 0;
+                    continue;
                 }
-                else {
-                    dp[i][j] = Math.min(dp[i - 1][j] + (int)s1.charAt(i - 1), dp[i][j -  1] + (int)s2.charAt(j - 1));
+                
+                if (index_i == 0) {
+                    dp[index_i][index_j] = dp[index_i][index_j - 1] + (int)s2.charAt(index_j - 1);
+                } else if (index_j == 0) {
+                    dp[index_i][index_j] = dp[index_i - 1][index_j] + (int)s1.charAt(index_i - 1);
+                } else if (s1.charAt(index_i - 1) == s2.charAt(index_j - 1)) {
+                    dp[index_i][index_j] = dp[index_i - 1][index_j - 1];
+                } else {
+                    dp[index_i][index_j] = Math.min((int)s1.charAt(index_i - 1) + dp[index_i - 1][index_j], 
+                                                   (int)s2.charAt(index_j - 1) + dp[index_i][index_j - 1]);
                 }
             }
         }
+        return dp[s1.length()][s2.length()];
+    }
+}
+
+class Solution {
+    public int minimumDeleteSum(String s1, String s2) {
+        this.s1 = s1;
+        this.s2 = s2;
         
-        return dp[s1.length()][s2.length()]; // 20 ms
+        dp = new Integer[this.s1.length() + 1][this.s2.length() + 1];
+        
+        recurse(0, 0);
+        
+        return dp[0][0];
     }
     
-    private int[][] dp;
+    private Integer[][] dp;
+    private String s1, s2;
     
-    private int recurse(String s1, String s2, int i, int j) {
-        int cost = 0;
-        if(dp[i][j] != -1) {
-            return dp[i][j];
+    private int getASCIISum(String str, int index) {
+        
+        int sumASCII = 0;
+        
+        while (index < str.length()) {
+            sumASCII += (int)str.charAt(index++);
         }
         
-        if(i == s1.length() && j == s2.length()) {
-            dp[i][j] = cost =  0;
-            
-            return dp[i][j];
-        }
-        else if(i == s1.length()) {
-            cost += recurse(s1, s2, i, j + 1) + (int)s2.charAt(j);    
-        }
-        else if(j == s2.length()) {
-            cost += recurse(s1, s2, i + 1, j) + (int)s1.charAt(i);
-        }
-        else if(s1.charAt(i) == s2.charAt(j)) {
-            cost += recurse(s1, s2, i + 1, j + 1);
-        }
-        else {
-            cost += Math.min(recurse(s1, s2, i, j + 1) + (int)s2.charAt(j), recurse(s1, s2, i + 1, j) + (int)s1.charAt(i));
+        return sumASCII;
+    }
+    
+    private int recurse(int index_i, int index_j) {
+        if (Objects.nonNull(dp[index_i][index_j])) {
+            return dp[index_i][index_j];
         }
         
-        dp[i][j] = cost;
-        return dp[i][j]; 
+        if (index_i == this.s1.length()) {
+            return dp[index_i][index_j] = getASCIISum(this.s2, index_j);
+        }
+        
+        if (index_j == this.s2.length()) {
+            return dp[index_i][index_j] = getASCIISum(this.s1, index_i);
+        }
+        
+        if (this.s1.charAt(index_i) == this.s2.charAt(index_j)) {
+            return dp[index_i][index_j] = recurse(index_i + 1, index_j + 1);
+        }
+        
+        return dp[index_i][index_j] = Math.min(recurse(index_i + 1, index_j) + (int)this.s1.charAt(index_i), 
+                                              recurse(index_i, index_j + 1) + (int)this.s2.charAt(index_j));
     }
 }
