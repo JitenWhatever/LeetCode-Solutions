a2c3557bdec103c0498c516c0494c89fb7a75c98
jitenwhatever <jitendra1551k@gmail.com>
2021-07-14 01:12:10 +0530

    dp + string

--- Medium/Word Break.java
+++ Medium/Word Break.java
@@ -1,26 +1,34 @@
 /*
-Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, 
-determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
 
-Note:
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
 
-The same word in the dictionary may be reused multiple times in the segmentation.
-You may assume the dictionary does not contain duplicate words.
+ 
+
 Example 1:
 
-Input: s = "leetcode", wordDict = ["leet", "code"]
+Input: s = "leetcode", wordDict = ["leet","code"]
 Output: true
 Explanation: Return true because "leetcode" can be segmented as "leet code".
 Example 2:
 
-Input: s = "applepenapple", wordDict = ["apple", "pen"]
+Input: s = "applepenapple", wordDict = ["apple","pen"]
 Output: true
 Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
-             Note that you are allowed to reuse a dictionary word.
+Note that you are allowed to reuse a dictionary word.
 Example 3:
 
-Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
 Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 300
+1 <= wordDict.length <= 1000
+1 <= wordDict[i].length <= 20
+s and wordDict[i] consist of only lowercase English letters.
+All the strings of wordDict are unique.
 */
 
 class Solution {
@@ -78,5 +86,27 @@ class Solution {
         }
         
         return false;
+    }
+}
+
+
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return wordBreakMemo(s, new HashSet<>(wordDict), 0, new Boolean[s.length()]);
+    }
+
+    private boolean wordBreakMemo(String s, Set<String> wordDict, int start, Boolean[] memo) {
+        if (start == s.length()) {
+            return true;
+        }
+        if (memo[start] != null) {
+            return memo[start];
+        }
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (wordDict.contains(s.substring(start, end)) && wordBreakMemo(s, wordDict, end, memo)) {
+                return memo[start] = true;
+            }
+        }
+        return memo[start] = false;
     }
 }
