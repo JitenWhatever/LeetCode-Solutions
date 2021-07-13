a8d0a38291817a58c3173e4631e1b4cae054aa57
jitenwhatever <jitendra1551k@gmail.com>
2021-07-14 01:44:58 +0530

    recursion + string

--- Hard/Word Break II.java
+++ Hard/Word Break II.java
@@ -1,76 +1,71 @@
 /*
-Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, 
-add spaces in s to construct a sentence where each word is a valid dictionary word. 
-Return all such possible sentences.
+Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. 
+Return all such possible sentences in any order.
 
-Note:
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
 
-The same word in the dictionary may be reused multiple times in the segmentation.
-You may assume the dictionary does not contain duplicate words.
 Example 1:
 
-Input:
-s = "catsanddog"
-wordDict = ["cat", "cats", "and", "sand", "dog"]
-Output:
-[
-  "cats and dog",
-  "cat sand dog"
-]
+Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
+Output: ["cats and dog","cat sand dog"]
 Example 2:
 
-Input:
-s = "pineapplepenapple"
-wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
-Output:
-[
-  "pine apple pen apple",
-  "pineapple pen apple",
-  "pine applepen apple"
-]
+Input: s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]
+Output: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
 Explanation: Note that you are allowed to reuse a dictionary word.
 Example 3:
 
-Input:
-s = "catsandog"
-wordDict = ["cats", "dog", "sand", "and", "cat"]
-Output:
-[]
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: []
+ 
+
+Constraints:
+
+1 <= s.length <= 20
+1 <= wordDict.length <= 1000
+1 <= wordDict[i].length <= 10
+s and wordDict[i] consist of only lowercase English letters.
+All the strings of wordDict are unique.
 */
 
 class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
-        HashSet<String> words = new HashSet<>(wordDict);
-        HashMap<Integer, List<String>> cache = new HashMap<>();
         
-        return wordBreakUtil(s, 0, words, cache);
+        this.cache = new HashMap<>();
+        this.wordDict = wordDict;
+        
+        return recurse(s, 0) ;
     }
     
-    private List<String> wordBreakUtil(String s, int left, Set<String> words, Map<Integer, List<String>> cache) {
-        
-        if(cache.containsKey(left)) {
-            return cache.get(left);
+    private Map<Integer, List<String>> cache;
+    private List<String> wordDict;
+    
+    private List<String> recurse(String s, int start) {
+        if (cache.containsKey(start)) {
+            return cache.get(start);
         }
         
         List<String> result = new ArrayList<>();
         
-        if(left == s.length()) { // string exhausted
-            result.add("");
+        if (start == s.length()) {
+           result.add("");
+            
+            return result;
         }
         
-        for(int right = left + 1; right <= s.length(); ++right) {
-            String prefix =  s.substring(left, right);
+        for (int end = start + 1; end <= s.length(); ++end) {
+            String prefix = s.substring(start, end);
             
-            if(words.contains(prefix)) {
-                List<String> suffixes = wordBreakUtil(s, right, words, cache);
-                
-                for(String suffix : suffixes) {
-                    result.add(prefix + (suffix.equals("") ? "" : " ") + suffix);
+            List<String> suffixes = recurse(s, end);
+            if (this.wordDict.contains(prefix)) {
+                for (String suffix : suffixes) {
+                    result.add(prefix + (suffix.isEmpty() ? "" : " ") + suffix);
                 }
             }
         }
         
-        cache.put(left, result);
+        cache.put(start, result);
+        
         return result;
     }
 }
