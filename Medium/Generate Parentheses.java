2475343ccd3d15c7fa221f8abe910427996c106c
jitenwhatever <jitendra1551k@gmail.com>
2021-07-20 00:42:00 +0530

    string + dp + backtrack

--- Medium/Generate Parentheses.java
+++ Medium/Generate Parentheses.java
@@ -17,3 +17,75 @@ Constraints:
 
 1 <= n <= 8
 */
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        this.result = new ArrayList<>();
+        
+        backtrack(new char[2*n], 0);
+        
+        return result;
+    }
+    
+    private List<String> result;
+    
+    private void backtrack(char[] currentResult, int index) {
+        if (index == currentResult.length) {
+            if (isvalid(currentResult)) {
+                result.add(new String(currentResult));
+            }
+        } else {
+            currentResult[index] = '(';
+            backtrack(currentResult, index + 1);
+            currentResult[index] = ')';
+            backtrack(currentResult, index + 1);
+        } 
+    }
+    
+    private boolean isvalid (char[] s) {
+        int balance = 0;
+        
+        for (char ch : s) {
+            if (ch == '(') {
+                ++balance;
+            } else {
+                --balance;
+            }
+            
+            if (balance < 0) {
+                return false;
+            }
+        }
+        
+        return balance == 0;
+    }
+}
+
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        this.result = new ArrayList<>();
+        
+        backtrack("", 0, 0, n);
+        
+        return result;
+    }
+    
+    private List<String> result;
+    
+    private void backtrack(String currentResult, int open, int close, int n) {
+        if (2*n == currentResult.length()) {
+            result.add(currentResult);
+            return;
+        } 
+        
+        if (open < n) {
+             backtrack(currentResult + "(", open + 1, close, n);   
+        }
+        
+        if (close < open) {
+            backtrack(currentResult + ")", open, close + 1, n);  
+        }
+    }
+    
+}
