03c3539ccdd5dded775ffbf7191c970b65a4ae45
jitenwhatever <jitendra1551k@gmail.com>
2021-07-23 06:28:12 +0530

    tree traversal + lt6

--- Medium/N-ary Tree Level Order Traversal.java
+++ Medium/N-ary Tree Level Order Traversal.java
@@ -72,3 +72,48 @@ class Solution {
         return levelOrder;
     }
 }
+
+class Solution {
+
+    private List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> levelOrder(Node root) {
+        if (root != null) traverseNode(root, 0);
+        return result;
+    }
+
+    private void traverseNode(Node node, int level) {
+        if (result.size() <= level) {
+            result.add(new ArrayList<>());
+        }
+        result.get(level).add(node.val);
+        for (Node child : node.children) {
+            traverseNode(child, level + 1);
+        }
+    }
+}
+
+// This code is a modified version of the code posted by
+// #zzzliu on the discussion forums.
+class Solution {
+
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        List<Node> previousLayer = Arrays.asList(root);
+
+        while (!previousLayer.isEmpty()) {
+            List<Node> currentLayer = new ArrayList<>();
+            List<Integer> previousVals = new ArrayList<>();
+            for (Node node : previousLayer) {
+                previousVals.add(node.val);
+                currentLayer.addAll(node.children);
+            }
+            result.add(previousVals);
+            previousLayer = currentLayer;
+        }
+
+        return result;
+    }
+}
