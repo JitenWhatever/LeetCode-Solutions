fa44932a9152dd7ecc878b942426f384ef70363f
jitenwhatever <jitendra1551k@gmail.com>
2021-07-25 04:01:49 +0530

    recurse + itr

--- Easy/Same Tree.java
+++ Easy/Same Tree.java
@@ -30,7 +30,41 @@ Input:     1         1
         [1,2,1],   [1,1,2]
 
 Output: false
+Constraints:
+
+The number of nodes in both trees is in the range [0, 100].
+-104 <= Node.val <= 104
 */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (Objects.isNull(p)) {
+            return Objects.isNull(q);
+        }
+        
+         if (Objects.isNull(q)) {
+            return Objects.isNull(p);
+        }
+        
+
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+
 
 /**
  * Definition for a binary tree node.
@@ -49,18 +83,50 @@ Output: false
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null) {
-            return true;
+
+        if (Objects.isNull(p)) {
+            return Objects.isNull(q);
+        }
+        if (Objects.isNull(q)) {
+            return Objects.isNull(p);
         }
         
-        if(p == null || q == null) {
-            return false;
+        boolean isSameTree = true;
+        
+        Stack<TreeNode> P = new Stack<>();
+        Stack<TreeNode> Q = new Stack<>();
+        P.push(p);
+        Q.push(q);
+        
+        while (!P.isEmpty()) {
+            p = P.pop();
+            q = Q.pop();
+            
+            if (!isEqual(p, q)) return false;
+            
+            if (!isEqual(p.left, q.left)) return false;
+            if (Objects.nonNull(p.left)) {
+                P.push(p.left);
+                Q.push(q.left);
+            }
+            if (!isEqual(p.right, q.right)) return false;
+            if (Objects.nonNull(p.right)) {
+                P.push(p.right);
+                Q.push(q.right);   
+            }
+            
         }
         
-        if(p.val != q.val) {
-            return false;
+        return isSameTree;
+    }
+    
+    private boolean isEqual(TreeNode p, TreeNode q) {
+        if (Objects.isNull(p)) {
+            return Objects.isNull(q);
         }
-        
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        if (Objects.isNull(q)) {
+            return Objects.isNull(p);
+        }
+        return p.val == q.val;
     }
 }
