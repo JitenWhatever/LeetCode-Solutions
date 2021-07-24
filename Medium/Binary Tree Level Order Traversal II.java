1224625ffdba81cc272d910eb2a9fcf6904bb5d3
jitenwhatever <jitendra1551k@gmail.com>
2021-07-24 21:06:18 +0530

    tree traversal + level order + reverse + recurse

--- Medium/Binary Tree Level Order Traversal II.java
+++ Medium/Binary Tree Level Order Traversal II.java
@@ -82,3 +82,29 @@ class Solution {
         return levelOrder;
     }
 }
+
+class Solution {
+    List<List<Integer>> levels = new ArrayList<List<Integer>>();
+
+    public void helper(TreeNode node, int level) {
+        // start the current level
+        if (levels.size() == level)
+            levels.add(new ArrayList<Integer>());
+
+         // append the current node value
+         levels.get(level).add(node.val);
+
+         // process child nodes for the next level
+         if (node.left != null)
+            helper(node.left, level + 1);
+         if (node.right != null)
+            helper(node.right, level + 1);
+    }
+    
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null) return levels;
+        helper(root, 0);
+        Collections.reverse(levels);
+        return levels;
+    }
+}
