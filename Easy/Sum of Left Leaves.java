87cf14a88018a477699315d1732063a604315894
jitenwhatever <jitendra1551k@gmail.com>
2021-07-27 00:25:46 +0530

    add path + DFS + itr

--- Easy/Sum of Left Leaves.java
+++ Easy/Sum of Left Leaves.java
@@ -1,7 +1,7 @@
 /*
 Find the sum of all left leaves in a given binary tree.
 
-Example:
+Example 1:
 
     3
    / \
@@ -9,7 +9,19 @@ Example:
     /  \
    15   7
 
-There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+Input: root = [3,9,20,null,null,15,7]
+Output: 24
+Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+Example 2:
+
+Input: root = [1]
+Output: 0
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+-1000 <= Node.val <= 1000
 */
 
 /**
@@ -44,4 +56,40 @@ class Solution {
         
         return sum;
     }
+}
+
+class Solution { 
+    
+  private boolean isLeaf(TreeNode node) {
+    return node != null && node.left == null && node.right == null;
+  }
+
+  public int sumOfLeftLeaves(TreeNode root) {
+
+    if (root == null) {
+      return 0;
+    }
+
+    int total = 0;
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    stack.push(root);
+
+    while (!stack.isEmpty()) {
+      TreeNode subRoot = stack.pop();
+      // Check if the left node is a leaf node.
+      if (isLeaf(subRoot.left)) {
+        total += subRoot.left.val;
+      }
+      // If the right node exists, put it on the stack.
+      if (subRoot.right != null) {    
+        stack.push(subRoot.right);
+      }
+      // If the left node exists, put it on the stack.
+      if (subRoot.left != null) {
+        stack.push(subRoot.left);
+      }
+    }
+
+    return total;
+  }
 }
