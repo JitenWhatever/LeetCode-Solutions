c0fdfaad528bb505af9ee3b7e3175ebec3c1aa53
jitenwhatever <jitendra1551k@gmail.com>
2021-07-30 22:31:51 +0530

    search BST

--- Easy/Search in a Binary Search Tree.java
+++ Easy/Search in a Binary Search Tree.java
@@ -3,16 +3,15 @@ Given the root node of a binary search tree (BST) and a value.
 You need to find the node in the BST that the node's value equals the given value. 
 Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
 
-For example, 
-
-Given the tree:
+Example 1:
         4
        / \
       2   7
      / \
     1   3
 
-And the value to search: 2
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
 
 You should return this subtree:
 
@@ -20,11 +19,24 @@ You should return this subtree:
      / \   
     1   3
 
-In the example above, if we want to search the value 5, 
-since there is no node with value 5, we should return NULL.
 
-Note: that an empty tree is represented by NULL, 
-therefore you would see the expected output (serialized tree format) as [], not null.
+Example 2:
+
+        4
+       / \
+      2   7
+     / \
+    1   3
+Input: root = [4,2,7,1,3], val = 5
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 5000].
+1 <= Node.val <= 107
+root is a binary search tree.
+1 <= val <= 107
 */
 
 /**
@@ -55,19 +67,18 @@ therefore you would see the expected output (serialized tree format) as [], not 
  * }
  */
 class Solution {
-    public TreeNode searchBST(TreeNode root, int val) {
+    public TreeNode searchBSTRecursive(TreeNode root, int val) {
         
-        if(root == null) {
+         if (Objects.isNull(root) || root.val == val) {
             return root;
         }
-        
-        if(root.val == val) {
+        return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val);
+    }
+   
+   public TreeNode searchBSTIterative(TreeNode root, int val) {
+        while (Objects.nonNUll(root) && val != root.val)
+            root = val < root.val ? root.left : root.right;
             return root;
         }
-        else if(root.val < val) {
-            return searchBST(root.right, val);
-        }
-        
-        return searchBST(root.left, val);
     }
 }
