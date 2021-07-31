cda28bf15e7a446af8a53ac2759b3463c445ad97
jitenwhatever <jitendra1551k@gmail.com>
2021-08-01 02:03:33 +0530

    paths sum in tree

--- Medium/Sum Root to Leaf Numbers.java
+++ Medium/Sum Root to Leaf Numbers.java
@@ -1,13 +1,14 @@
 /*
-Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+You are given the root of a binary tree containing digits from 0 to 9 only.
 
-An example is the root-to-leaf path 1->2->3 which represents the number 123.
+Each root-to-leaf path in the tree represents a number.
 
-Find the total sum of all root-to-leaf numbers.
+For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
 
-Note: A leaf is a node with no children.
+A leaf node is a node with no children.
 
-Example:
+Example 1:
 
 Input: [1,2,3]
     1
@@ -18,6 +19,7 @@ Explanation:
 The root-to-leaf path 1->2 represents the number 12.
 The root-to-leaf path 1->3 represents the number 13.
 Therefore, sum = 12 + 13 = 25.
+
 Example 2:
 
 Input: [4,9,0,5,1]
@@ -33,6 +35,12 @@ The root-to-leaf path 4->9->5 represents the number 495.
 The root-to-leaf path 4->9->1 represents the number 491.
 The root-to-leaf path 4->0 represents the number 40.
 Therefore, sum = 495 + 491 + 40 = 1026.
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+0 <= Node.val <= 9
+The depth of the tree will not exceed 10.
 */
 
 /**
@@ -52,29 +60,54 @@ Therefore, sum = 495 + 491 + 40 = 1026.
  */
 class Solution {
     public int sumNumbers(TreeNode root) {
-        if(root == null) {
-            return 0;
+        this.pathsSum = 0;
+        
+        if (Objects.isNull(root)) {
+            return this.pathsSum;
         }
         
-        dfs(root, root.val);
+        int currentPathSum = 0;
         
-        return totalSum;
+        Stack<Pair<TreeNode, Integer>> stck = new Stack<>();
+        
+        stck.push(new Pair(root, 0));
+        
+        while (!stck.isEmpty()) {
+            Pair<TreeNode, Integer> currentNode = stck.pop();
+            root = currentNode.getKey();
+            currentPathSum = currentNode.getValue()*10 + root.val;
+            
+            if (Objects.isNull(root.left) && Objects.isNull(root.right)) {
+                this.pathsSum +=  currentPathSum;
+            }
+            
+            if (Objects.nonNull(root.left)) {
+                stck.push(new Pair(root.left, currentPathSum));
+            }
+            
+            if (Objects.nonNull(root.right)) {
+                stck.push(new Pair(root.right, currentPathSum));
+            }
+            
+        }
+        
+        return this.pathsSum;
     }
     
-    private int totalSum = 0;
-    private void dfs(TreeNode root, int sum) {
-        
-        if(root.left == null && root.right == null) {
-            totalSum += sum;
+    private int pathsSum;
+    
+    private  void pathsSumRecursive(TreeNode root, int currentPathSum) {
+        if (Objects.isNull(root)) {
             return ;
         }
         
-        if(root.left != null) {
-            dfs(root.left, sum*10 + root.left.val);
+        currentPathSum = currentPathSum * 10 + root.val;
+        if (Objects.isNull(root.left) && Objects.isNull(root.right)) {
+            this.pathsSum += currentPathSum;
+            return ;
         }
         
-        if(root.right != null) {
-            dfs(root.right, sum*10 + root.right.val);
-        }
+        pathsSum(root.left, currentPathSum);             
+        pathsSum(root.right, currentPathSum);
     }
 }
