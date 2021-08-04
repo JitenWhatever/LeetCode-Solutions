89e8a16b3accfb116587efe80a722582ab9937e3
jitenwhatever <jitendra1551k@gmail.com>
2021-08-04 23:28:26 +0530

    dfs + backtrack

--- Medium/Path Sum II.java
+++ Medium/Path Sum II.java
@@ -43,119 +43,36 @@ The number of nodes in the tree is in the range [0, 5000].
  * }
  */
 class Solution {
-    public List<String> binaryTreePaths(TreeNode root) {
-        List<String> paths = new ArrayList<>();
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        this.paths = new ArrayList<>();
         
-        if (Objects.isNull(root)) {
-            return paths;
-        }
+        getPaths(root, targetSum, new ArrayList<>());
         
-        StringBuilder currentPath = new StringBuilder();
-        findPaths(root, paths, currentPath);
-        
-        return paths;
+        return this.paths;
     }
     
-    private void findPaths(TreeNode root, List<String> paths, StringBuilder currentPath) {
+    private List<List<Integer>> paths;
+    
+    private void getPaths(TreeNode root, int target, List<Integer> currentPath) {
         if (Objects.isNull(root)) {
-            return ;
+            return;
         }
-        currentPath.append(root.val);
-        if (Objects.isNull(root.left) && Objects.isNull(root.right)) {
-            paths.add(currentPath.toString());
+        currentPath.add(root.val);
+        target -= root.val;
+        if (Objects.isNull(root.left) && Objects.isNull(root.right) && target == 0) {
+            this.paths.add(new ArrayList<>(currentPath));
             return;
         }
         
-        currentPath.append("->");
-        findPaths(root.left, paths, new StringBuilder(currentPath.toString()));
-        findPaths(root.right, paths, new StringBuilder(currentPath.toString()));
+        if (Objects.nonNull(root.left)) {
+              getPaths(root.left, target, currentPath);
+            currentPath.remove(currentPath.size() - 1);
+        }
         
+        if (Objects.nonNull(root.right)) {
+            getPaths(root.right, target, currentPath);
+            currentPath.remove(currentPath.size() - 1);
+        }
+      
     }
 }
-
-class Solution {
-    public List<String> binaryTreePaths(TreeNode root) {
-        List<String> paths = new ArrayList<>();
-        
-        if (Objects.isNull(root)) {
-            return paths;
-        }
-        
-        StringBuilder currentPath = new StringBuilder();
-        findPaths(root, paths, currentPath);
-        
-        return paths;
-    }
-    
-    private void findPaths(TreeNode root, List<String> paths, StringBuilder currentPath) {
-        if (Objects.isNull(root)) {
-            return ;
-        }
-        currentPath.append(root.val);
-        if (Objects.isNull(root.left) && Objects.isNull(root.right)) {
-            paths.add(currentPath.toString());
-            return;
-        }
-        
-        currentPath.append("->");
-        int len = currentPath.length();
-        findPaths(root.left, paths, currentPath);
-        currentPath.setLength(len);
-        findPaths(root.right, paths, currentPath);
-        currentPath.setLength(len);
-    }
-
-    // BFS
-    public List<String> binaryTreePathsBFS(TreeNode root) {
-        List<String> list = new ArrayList<String>();
-        Queue<TreeNode> qNode = new LinkedList<TreeNode>();
-        Queue<String> qStr = new LinkedList<String>();
-        
-        if (root==null) return list;
-
-        qNode.add(root);
-        qStr.add("");
-        while(!qNode.isEmpty()) {
-            TreeNode curNode=qNode.poll();
-            String curStr=qStr.poll();
-            
-            if (curNode.left==null && curNode.right==null) list.add(curStr+curNode.val);
-            if (curNode.left!=null) {
-                qNode.add(curNode.left);
-                qStr.add(curStr+curNode.val+"->");
-            }
-            if (curNode.right!=null) {
-                qNode.add(curNode.right);
-                qStr.add(curStr+curNode.val+"->");
-            }
-        }
-        return list;
-    }
-
-    // DFS
-    public List<String> binaryTreePathsDFS(TreeNode root) {
-        List<String> list=new ArrayList<String>();
-        Stack<TreeNode> sNode=new Stack<TreeNode>();
-        Stack<String> sStr=new Stack<String>();
-        
-        if(root==null) return list;
-        sNode.push(root);
-        sStr.push("");
-        while(!sNode.isEmpty()) {
-            TreeNode curNode=sNode.pop();
-            String curStr=sStr.pop();
-            
-            if(curNode.left==null && curNode.right==null) list.add(curStr+curNode.val);
-            if(curNode.left!=null) {
-                sNode.push(curNode.left);
-                sStr.push(curStr+curNode.val+"->");
-            }
-            if(curNode.right!=null) {
-                sNode.push(curNode.right);
-                sStr.push(curStr+curNode.val+"->");
-            }
-        }
-        return list;
-    }
-}
-
