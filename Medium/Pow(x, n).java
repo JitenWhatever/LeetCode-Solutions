ea1887d763b7b8472727a50dae4ffc4fce2484cb
jitenwhatever <jitendra1551k@gmail.com>
2021-08-08 02:57:51 +0530

    binary exponentiation

--- Medium/Pow(x, n).java
+++ Medium/Pow(x, n).java
@@ -1,44 +1,80 @@
 /*
-Implement pow(x, n), which calculates x raised to the power n (xn).
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
 
 Example 1:
 
-Input: 2.00000, 10
+Input: x = 2.00000, n = 10
 Output: 1024.00000
 Example 2:
 
-Input: 2.10000, 3
+Input: x = 2.10000, n = 3
 Output: 9.26100
 Example 3:
 
-Input: 2.00000, -2
+Input: x = 2.00000, n = -2
 Output: 0.25000
 Explanation: 2-2 = 1/22 = 1/4 = 0.25
-Note:
+ 
+
+Constraints:
 
 -100.0 < x < 100.0
-n is a 32-bit signed integer, within the range [−231, 231 − 1]
+-231 <= n <= 231-1
+-104 <= xn <= 104
 */
 
 
 class Solution {
     public double myPow(double x, int n) {
-        if(n < 0) {
-            x = 1/x;
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
         }
-        
-        double result = 1, power = x;
-        while(n != 0) {
-            if(n % 2 !=  0) {
-                result *= power;
-            }
-            
-            n /= 2;
-            
-            power *= power; 
+        double ans = 1;
+        for (long i = 0; i < N; i++)
+            ans = ans * x;
+        return ans;
+    }
+};
+
+class Solution {
+    private double fastPow(double x, long n) {
+        if (n == 0) {
+            return 1.0;
         }
-        
-        
-        return result;
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return fastPow(x, N);
+    }
+
+    double myPow(double x, int n){
+    
+    if (n == 0){
+        return 1;
+    } else {  
+        double half = myPow(x, n/2);
+        if (n%2 == 0){
+            return half*half;
+        } else {
+            if (n > 0){
+                return half*half*x;
+            } else {
+                return half*half/x;
+            }                
+        }   
     }
 }
+};
