d2ddef8bc07c735617915d8cd68a54eb0ec0e1e3
jitenwhatever <jitendra1551k@gmail.com>
2021-08-08 06:13:48 +0530

    start from end find nums[i] > nums[i - 1]

---
+++ Medium/Next Permutation.java
@@ -0,0 +1,67 @@
+/*
+Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+
+If such an arrangement is not possible, it must rearrange it as the lowest possible order (i.e., sorted in ascending order).
+
+The replacement must be in place and use only constant extra memory.
+
+ 
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: nums = [3,2,1]
+Output: [1,2,3]
+Example 3:
+
+Input: nums = [1,1,5]
+Output: [1,5,1]
+Example 4:
+
+Input: nums = [1]
+Output: [1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 100
+*/
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int index = nums.length - 1;
+        
+        while (index > 0 && nums[index] <= nums[index - 1]) {
+            --index;
+        }
+        
+        if (index == 0) {
+            Arrays.sort(nums);
+            return ;
+        }
+        
+        
+        int big = index, temp = index;
+        index = nums.length - 1;
+        while (index > big) {
+            if (nums[index] > nums[big - 1] && nums[index] < nums[temp]) {
+                temp = index;
+            }
+            --index;
+        }
+        
+        swap(nums, big - 1, temp);
+        
+        Arrays.sort(nums, big, nums.length);
+        
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
