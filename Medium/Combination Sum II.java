cb447deea4d4029258858e94a0ba3df85bc3f05d
jitenwhatever <jitendra1551k@gmail.com>
2021-08-09 23:35:36 +0530

    same element can't be use twice and input contains duplicates and result should not

--- Medium/Combination Sum II.java
+++ Medium/Combination Sum II.java
@@ -1,60 +1,65 @@
 /*
-Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+Given a collection of candidate numbers (candidates) and a target number (target), 
+find all unique combinations in candidates where the candidate numbers sum to target.
 
 Each number in candidates may only be used once in the combination.
 
-Note:
+Note: The solution set must not contain duplicate combinations.
 
-All numbers (including target) will be positive integers.
-The solution set must not contain duplicate combinations.
+ 
+
 Example 1:
 
-Input: candidates = [10,1,2,7,6,1,5], target = 8,
-A solution set is:
+Input: candidates = [10,1,2,7,6,1,5], target = 8
+Output: 
 [
-  [1, 7],
-  [1, 2, 5],
-  [2, 6],
-  [1, 1, 6]
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
 ]
 Example 2:
 
-Input: candidates = [2,5,2,1,2], target = 5,
-A solution set is:
+Input: candidates = [2,5,2,1,2], target = 5
+Output: 
 [
-  [1,2,2],
-  [5]
+[1,2,2],
+[5]
 ]
+ 
+
+Constraints:
+
+1 <= candidates.length <= 100
+1 <= candidates[i] <= 50
+1 <= target <= 30
 */
 
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        
-        result = new ArrayList<>();
-        List<Integer> current = new ArrayList<>();
+        List<List<Integer>> combinations = new ArrayList<>();
         
         Arrays.sort(candidates);
-        generate(candidates, 0, target, current);
-        return result;
+        backtrackCombinations(candidates, target, 0, new ArrayList<>(), combinations);
+        
+        return combinations;
     }
     
-    private List<List<Integer>> result;
-    
-    private void generate(int[] candidates, int index, int target, List<Integer> current) {
-        if(target == 0) {
-            result.add(new ArrayList(current));
+     private void backtrackCombinations(int[] nums, int remainingSum, int start , List<Integer> currentCombination, List<List<Integer>> combinations) {
+        if (remainingSum == 0) {
+            combinations.add(new ArrayList<>(currentCombination));
             return ;
         }
         
-        if(target < 0) {
+        if (remainingSum < 0) { // current number cannot be used
             return ;
         }
         
-        for(int i = index; i < candidates.length; ++i) {
-            if(i == index || candidates[i] != candidates[i - 1]) {
-                current.add(candidates[i]);
-                generate(candidates, i + 1, target - candidates[i], current);
-                current.remove(current.size() - 1);
+        for (int index = start; index < nums.length; ++index) {
+            if (index == start || nums[index] != nums[index - 1]) {
+                currentCombination.add(nums[index]);
+                backtrackCombinations(nums, remainingSum - nums[index], index + 1, currentCombination, combinations);
+                currentCombination.remove(currentCombination.size() - 1);
             }
         }
     }
