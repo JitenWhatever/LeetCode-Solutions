0c91a0987c455600c1fd9af9b00a76cb2862d525
jitenwhatever <jitendra1551k@gmail.com>
2021-08-13 22:54:13 +0530

    iterate + dfs for sparse matrix

--- Easy/Island Perimeter.java
+++ Easy/Island Perimeter.java
@@ -1,11 +1,11 @@
 /*
-You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water.
+You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
 
-Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, 
+and there is exactly one island (i.e., one or more connected land cells).
 
-The island doesn't have "lakes" (water inside that isn't connected to the water around the island). One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
-
- 
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. 
+The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
 
 Example:
 
@@ -15,9 +15,25 @@ Input:
  [0,1,0,0],
  [1,1,0,0]]
 
+Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
 Output: 16
+Explanation: The perimeter is the 16 yellow stripes in the image above.
+Example 2:
 
-Explanation: The perimeter is the 16 yellow stripes in the image below:
+Input: grid = [[1]]
+Output: 4
+Example 3:
+
+Input: grid = [[1,0]]
+Output: 4
+ 
+
+Constraints:
+
+row == grid.length
+col == grid[i].length
+1 <= row, col <= 100
+grid[i][j] is 0 or 1.
 
 https://assets.leetcode.com/uploads/2018/10/12/island.png
 
@@ -25,23 +41,17 @@ https://assets.leetcode.com/uploads/2018/10/12/island.png
 
 class Solution {
     public int islandPerimeter(int[][] grid) {
-        
-        if(grid.length == 0 ) return 0;
-        
-        int H = grid.length, W = grid[0].length;
-        
-        int perimeter = 0;
-        
-        for(int r = 0; r < H; ++r) {
-            for(int c = 0; c < W; ++c) {
-                if(grid[r][c] == 1) {
+        int perimeter = 0;       
+        for (int row = 0; row < grid.length; ++row) {
+            for (int col = 0; col < grid[0].length; ++col) {
+                if (grid[row][col] == 1) {
                     perimeter += 4;
-                
-                    if(r > 0 && grid[r - 1][c] == 1) {
+                    
+                    if (row > 0 && grid[row - 1][col] == 1) {
                         perimeter -= 2;
                     }
-                
-                    if(c > 0 && grid[r][c - 1] == 1) {
+                    
+                    if (col > 0 && grid[row][col - 1] == 1) {
                         perimeter -= 2;
                     }
                 }
