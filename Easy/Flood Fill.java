6e1eced10cd351ed41e28a95ff9159cafd4d0ca6
jitenwhatever <jitendra1551k@gmail.com>
2021-08-13 23:00:00 +0530

    dfs

--- Easy/Flood Fill.java
+++ Easy/Flood Fill.java
@@ -1,7 +1,9 @@
 /*
  An image is represented by a 2-D array of integers, each integer representing the pixel value of the image (from 0 to 65535).
 Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill, and a pixel value newColor, "flood fill" the image.
-To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color as the starting pixel), and so on. Replace the color of all of the aforementioned pixels with the newColor.
+To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, 
+plus any pixels connected 4-directionally to those pixels (also with the same color as the starting pixel), and so on. 
+Replace the color of all of the aforementioned pixels with the newColor.
 
 At the end, return the modified image.
 
@@ -14,13 +16,24 @@ Output: [[2,2,2],[2,2,0],[2,0,1]]
 Explanation: 
 From the center of the image (with position (sr, sc) = (1, 1)), all pixels connected 
 by a path of the same color as the starting pixel are colored with the new color.
+
 Note the bottom corner is not colored 2, because it is not 4-directionally connected
 to the starting pixel.
 
-Note:
-The length of image and image[0] will be in the range [1, 50].
-The given starting pixel will satisfy 0 <= sr < image.length and 0 <= sc < image[0].length.
-The value of each color in image[i][j] and newColor will be an integer in [0, 65535].
+Example 2:
+
+Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, newColor = 2
+Output: [[2,2,2],[2,2,2]]
+ 
+
+Constraints:
+
+m == image.length
+n == image[i].length
+1 <= m, n <= 50
+0 <= image[i][j], newColor < 216
+0 <= sr < m
+0 <= sc < n
 */
 
 class Solution {
