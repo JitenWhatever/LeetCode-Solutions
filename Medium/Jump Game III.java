0b95de57ae8dbb4963fa127d9db1880a669efb31
jitenwhatever <jitendra1551k@gmail.com>
2021-08-17 21:23:01 +0530

    bfs

--- Medium/Jump Game III.java
+++ Medium/Jump Game III.java
@@ -46,3 +46,34 @@ class Solution {
         return false;
     }
 }
+
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        int n = arr.length;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            // check if reach zero
+            if (arr[node] == 0) {
+                return true;
+            }
+            if (arr[node] < 0) {
+                continue;
+            }
+
+            // check available next steps
+            if (node + arr[node] < n) {
+                q.offer(node + arr[node]);
+            }
+            if (node - arr[node] >= 0) {
+                q.offer(node - arr[node]);
+            }
+            // mark as visited
+            arr[node] = -arr[node];
+        }
+        return false;
+    }
+}
