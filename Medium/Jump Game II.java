8b59024b011e3143e41885fb6814ea43d83b0cb1
jitenwhatever <jitendra1551k@gmail.com>
2021-08-17 21:28:56 +0530

    dp

--- Medium/Jump Game II.java
+++ Medium/Jump Game II.java
@@ -57,3 +57,26 @@ class Solution {
 	}
 	return steps;
 }
+
+
+class Solution {
+    public int jump(int[] arr) {
+        int[] jump = new int[arr.length];
+    
+        Arrays.fill(jump, Integer.MAX_VALUE - 1);
+        jump[0] = 0;
+        
+        for(int i=1; i < arr.length; i++){
+            for(int j=0; j < i; j++){
+                if(arr[j] + j >= i){
+                    if(jump[i] > jump[j] + 1){
+                        result[i] = j;
+                        jump[i] = jump[j] + 1;
+                    }
+                }
+            }
+        }
+        
+        return jump[jump.length-1];
+    }
+}
