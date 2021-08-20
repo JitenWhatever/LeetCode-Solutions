506f509cacc92fc3796d46ff13464ec827e5a9be
jitenwhatever <jitendra1551k@gmail.com>
2021-08-20 23:02:33 +0530

    bst

--- Medium/Search in Rotated Sorted Array.java
+++ Medium/Search in Rotated Sorted Array.java
@@ -1,23 +1,37 @@
 /*
-Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+There is an integer array nums sorted in ascending order (with distinct values).
 
-(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+Prior to being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) 
+such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). 
+For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
 
-You are given a target value to search. If found in the array return its index, otherwise return -1.
+Given the array nums after the rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 
-You may assume no duplicate exists in the array.
+You must write an algorithm with O(log n) runtime complexity.
 
-Your algorithm's runtime complexity must be in the order of O(log n).
+ 
 
 Example 1:
 
 Input: nums = [4,5,6,7,0,1,2], target = 0
 Output: 4
-
 Example 2:
 
 Input: nums = [4,5,6,7,0,1,2], target = 3
 Output: -1
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+ 
+
+Constraints:
+
+1 <= nums.length <= 5000
+-104 <= nums[i] <= 104
+All values of nums are unique.
+nums is guaranteed to be rotated at some pivot.
+-104 <= target <= 104
 */
 
 class Solution {
