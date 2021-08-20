5c04b441ea1ecc5b84b5d9f183dc9287e0de2170
jitenwhatever <jitendra1551k@gmail.com>
2021-08-20 23:12:50 +0530

    binary search =

--- Medium/Search in Rotated Sorted Array.java
+++ Medium/Search in Rotated Sorted Array.java
@@ -97,3 +97,15 @@ class Solution {
     return -1;
   }
 }
+
+/*
+Formula: If a sorted array is shifted, if you take the middle, always one side will be sorted. Take the recursion according to that rule.
+
+1- take the middle and compare with target, if matches return.
+2- if middle is bigger than left side, it means left is sorted
+2a- if [left] < target < [middle] then do recursion with left, middle - 1 (right)
+2b- left side is sorted, but target not in here, search on right side middle + 1 (left), right
+3- if middle is less than right side, it means right is sorted
+3a- if [middle] < target < [right] then do recursion with middle + 1 (left), right
+3b- right side is sorted, but target not in here, search on left side left, middle -1 (right)
+*/
