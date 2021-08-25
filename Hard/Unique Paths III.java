bc1fb9919ab2660b8177e156b2493d0ba964307e
jitenwhatever <jitendra1551k@gmail.com>
2021-08-25 23:18:33 +0530

    dfs

--- Hard/Unique Paths III.java
+++ Hard/Unique Paths III.java
@@ -1,24 +1,31 @@
 /*
-On a 2-dimensional grid, there are 4 types of squares:
+You are given an m x n integer array grid where grid[i][j] could be:
 
-1 represents the starting square.  There is exactly one starting square.
-2 represents the ending square.  There is exactly one ending square.
-0 represents empty squares we can walk over.
--1 represents obstacles that we cannot walk over.
+1 representing the starting square. There is exactly one starting square.
+2 representing the ending square. There is exactly one ending square.
+0 representing empty squares we can walk over.
+-1 representing obstacles that we cannot walk over.
 Return the number of 4-directional walks from the starting square to the ending square, that walk over every non-obstacle square exactly once.
 
  
 
 Example 1:
-
-Input: [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+Input: [
+    [1,0,0,0],
+    [0,0,0,0],
+    [0,0,2,-1]
+    ]
 Output: 2
 Explanation: We have the following two paths: 
 1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)
 2. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2)
 Example 2:
 
-Input: [[1,0,0,0],[0,0,0,0],[0,0,0,2]]
+Input: [
+    [1,0,0,0],
+    [0,0,0,0],
+    [0,0,0,2]
+    ]
 Output: 4
 Explanation: We have the following four paths: 
 1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2),(2,3)
@@ -27,16 +34,24 @@ Explanation: We have the following four paths:
 4. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2),(2,3)
 Example 3:
 
-Input: [[0,1],[2,0]]
+Input: [
+    [0,1],
+    [2,0]
+    ]
 Output: 0
 Explanation: 
 There is no path that walks over every empty square exactly once.
 Note that the starting and ending square can be anywhere in the grid.
  
 
-Note:
+Constraints:
 
-1 <= grid.length * grid[0].length <= 20
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 20
+1 <= m * n <= 20
+-1 <= grid[i][j] <= 2
+There is exactly one starting cell and one ending cell.
 */
 
 class Solution {
