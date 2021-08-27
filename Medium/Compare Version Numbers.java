016b39cdda8427f60728e0a2bc5ec345b35a8549
jitenwhatever <jitendra1551k@gmail.com>
2021-08-27 22:51:01 +0530

    find each number and compare

--- Medium/Compare Version Numbers.java
+++ Medium/Compare Version Numbers.java
@@ -1,75 +1,84 @@
 /*
-Compare two version numbers version1 and version2.
-If version1 > version2 return 1; if version1 < version2 return -1;otherwise return 0.
+Given two version numbers, version1 and version2, compare them.
 
-You may assume that the version strings are non-empty and contain only digits and the . character.
+Version numbers consist of one or more revisions joined by a dot '.'. 
+Each revision consists of digits and may contain leading zeros. Every revision contains at least one character.
+ Revisions are 0-indexed from left to right, with the leftmost revision being revision 0, the next revision being revision 1, and so on. 
+ For example 2.5.33 and 0.1 are valid version numbers.
 
-The . character does not represent a decimal point and is used to separate number sequences.
+To compare version numbers, compare their revisions in left-to-right order.
+ Revisions are compared using their integer value ignoring any leading zeros. 
+ This means that revisions 1 and 001 are considered equal. 
+ If a version number does not specify a revision at an index, then treat the revision as 0. 
+ For example, version 1.0 is less than version 1.1 because their revision 0s are the same, but their revision 1s are 0 and 1 respectively, and 0 < 1.
 
-For instance, 2.5 is not "two and a half" or "half way to version three", it is the fifth second-level revision of the second first-level revision.
+Return the following:
 
-You may assume the default revision number for each level of a version number to be 0. 
-For example, version number 3.4 has a revision number of 3 and 4 for its first and second level revision number. Its third and fourth level revision number are both 0.
-
+If version1 < version2, return -1.
+If version1 > version2, return 1.
+Otherwise, return 0.
  
 
 Example 1:
 
-Input: version1 = "0.1", version2 = "1.1"
-Output: -1
-Example 2:
-
-Input: version1 = "1.0.1", version2 = "1"
-Output: 1
-Example 3:
-
-Input: version1 = "7.5.2.4", version2 = "7.5.3"
-Output: -1
-Example 4:
-
 Input: version1 = "1.01", version2 = "1.001"
 Output: 0
-Explanation: Ignoring leading zeroes, both “01” and “001" represent the same number “1”
-Example 5:
+Explanation: Ignoring leading zeroes, both "01" and "001" represent the same integer "1".
+Example 2:
 
 Input: version1 = "1.0", version2 = "1.0.0"
 Output: 0
-Explanation: The first version number does not have a third level revision number, which means its third level revision number is default to "0"
+Explanation: version1 does not specify revision 2, which means it is treated as "0".
+Example 3:
+
+Input: version1 = "0.1", version2 = "1.1"
+Output: -1
+Explanation: version1's revision 0 is "0", while version2's revision 0 is "1". 0 < 1, so version1 < version2.
+Example 4:
+
+Input: version1 = "1.0.1", version2 = "1"
+Output: 1
+Example 5:
+
+Input: version1 = "7.5.2.4", version2 = "7.5.3"
+Output: -1
  
 
-Note:
+Constraints:
 
-Version strings are composed of numeric strings separated by dots . and this numeric strings may have leading zeroes.
-Version strings do not start or end with dots, and they will not be two consecutive dots.
+1 <= version1.length, version2.length <= 500
+version1 and version2 only contain digits and '.'.
+version1 and version2 are valid version numbers.
+All the given revisions in version1 and version2 can be stored in a 32-bit integer.
 */
 
 
 class Solution {
-    public int compareVersion(String v1, String v2) {
-    
-        int i = 0;
-        int j = 0;
-        while(i < v1.length() || j < v2.length()) {
-            
-            int num1 = 0;
-            int num2 = 0;
-            
-            while(i < v1.length() && v1.charAt(i) != '.') {
-                num1 = 10* num1 + (v1.charAt(i++) - '0');
-            }
-            
-            while(j < v2.length() && v2.charAt(j) != '.') {
-                num2 = 10* num2 + (v2.charAt(j++) - '0');
-            }
+    public int compareVersion(String version1, String version2) {
+        int N = version1.length(), M = version2.length();
+        int index_1 = 0, index_2 = 0;
+        int version_1 = 0, version_2 = 0;
         
-            if(num1 > num2) {
+        while (index_1 < N || index_2 < M) {
+            while(index_1 < N && version1.charAt(index_1) != '.') {
+                version_1 = version_1 * 10 + (version1.charAt(index_1++) - '0');
+            }
+            
+             while(index_2 < M && version2.charAt(index_2) != '.') {
+                version_2 = version_2 * 10 + (version2.charAt(index_2++) - '0');
+            }
+            if (version_1 > version_2) {
                 return 1;
             }
-            else if(num1 < num2) {
+            
+            if (version_2 > version_1) {
                 return -1;
             }
-            i++;
-            j++;
+            
+            ++index_1;
+            ++index_2;
+            version_1 = 0;
+            version_2 = 0;
         }
         
         return 0;
