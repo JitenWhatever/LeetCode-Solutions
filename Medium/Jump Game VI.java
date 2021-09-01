73a62b6a418de9cf09856489187b4d52e4c90ea5
jitenwhatever <jitendra1551k@gmail.com>
2021-09-01 22:41:04 +0530

    dp + max sliding window + segment tree

--- Medium/Jump Game VI.java
+++ Medium/Jump Game VI.java
@@ -33,6 +33,7 @@ Constraints:
 -104 <= nums[i] <= 104
 */
 
+// time O(N), space O(N + k)
 class Solution {
     public int maxResult(int[] nums, int k) {
         int size = nums.length;
@@ -60,3 +61,98 @@ class Solution {
     }
 }
 
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        int n = nums.length;
+        int score = nums[0];
+        Deque<int[]> dq = new LinkedList<>();
+        dq.offerLast(new int[] { 0, score });
+        for (int i = 1; i < n; i++) {
+            // pop the old index
+            while (dq.peekFirst() != null && dq.peekFirst()[0] < i - k) {
+                dq.pollFirst();
+            }
+            score = dq.peek()[1] + nums[i];
+            // pop the smaller value
+            while (dq.peekLast() != null && score >= dq.peekLast()[1]) {
+                dq.pollLast();
+            }
+            dq.offerLast(new int[] { i, score });
+        }
+        return score;
+    }
+}
+
+// time O(NlogN), space( O(2N))
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        int n = nums.length;
+        int[] score = new int[n];
+        score[0] = nums[0];
+        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        priorityQueue.add(new int[] { nums[0], 0 });
+        for (int i = 1; i < n; i++) {
+            // pop the old index
+            while (priorityQueue.peek()[1] < i - k) {
+                priorityQueue.remove();
+            }
+            score[i] = nums[i] + score[priorityQueue.peek()[1]];
+            priorityQueue.add(new int[] { score[i], i });
+        }
+        return score[n - 1];
+    }
+}
+
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        int n = nums.length;
+        int score = nums[0];
+        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        priorityQueue.add(new int[] { nums[0], 0 });
+        for (int i = 1; i < n; i++) {
+            // pop the old index
+            while (priorityQueue.peek()[1] < i - k) {
+                priorityQueue.remove();
+            }
+            score = nums[i] + priorityQueue.peek()[0];
+            priorityQueue.add(new int[] { score, i });
+        }
+        return score;
+    }
+}
+
+
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        int n = nums.length;
+        int[] tree = new int[2 * n];
+        update(0, nums[0], tree, n);
+        for (int i = 1; i < n; i++) {
+            int maxi = query(Math.max(0, i - k), i, tree, n);
+            update(i, maxi + nums[i], tree, n);
+        }
+        return tree[2 * n - 1];
+    }
+
+    // implement Segment Tree
+    private void update(int index, int value, int[] tree, int n) {
+        index += n;
+        tree[index] = value;
+        for (index >>= 1; index > 0; index >>= 1) {
+            tree[index] = Math.max(tree[index << 1], tree[(index << 1) + 1]);
+        }
+    }
+
+    private int query(int left, int right, int[] tree, int n) {
+        int result = Integer.MIN_VALUE;
+        for (left += n, right += n; left < right; left >>= 1, right >>= 1) {
+            if ((left & 1) == 1) {
+                result = Math.max(result, tree[left++]);
+            }
+            if ((right & 1) == 1) {
+                result = Math.max(result, tree[--right]);
+            }
+        }
+        return result;
+    }
+}
