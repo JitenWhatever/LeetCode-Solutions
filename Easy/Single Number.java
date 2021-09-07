ff4a6b3bbcdaec10dba18116cf9a580d134dae55
jitenwhatever <jitendra1551k@gmail.com>
2021-09-07 21:21:46 +0530

    bit manipulation

--- Easy/Single Number.java
+++ Easy/Single Number.java
@@ -1,18 +1,29 @@
 /*
-Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
-Note:
-Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
 
 Example 1:
 
-Input: [2,2,1]
+Input: nums = [2,2,1]
 Output: 1
-
 Example 2:
 
-Input: [4,1,2,1,2]
+Input: nums = [4,1,2,1,2]
 Output: 4
+Example 3:
+
+Input: nums = [1]
+Output: 1
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
 */
 
 class Solution {
@@ -24,4 +35,51 @@ class Solution {
         
         return singleNumber;
     }
+}
+
+class Solution {
+  public int singleNumber(int[] nums) {
+    List<Integer> no_duplicate_list = new ArrayList<>();
+
+    for (int i : nums) {
+      if (!no_duplicate_list.contains(i)) {
+        no_duplicate_list.add(i);
+      } else {
+        no_duplicate_list.remove(new Integer(i));
+      }
+    }
+    return no_duplicate_list.get(0);
+  }
+}
+
+class Solution {
+  public int singleNumber(int[] nums) {
+    HashMap<Integer, Integer> hash_table = new HashMap<>();
+
+    for (int i : nums) {
+      hash_table.put(i, hash_table.getOrDefault(i, 0) + 1);
+    }
+    for (int i : nums) {
+      if (hash_table.get(i) == 1) {
+        return i;
+      }
+    }
+    return 0;
+  }
+}
+
+class Solution {
+  public int singleNumber(int[] nums) {
+    int sumOfSet = 0, sumOfNums = 0;
+    Set<Integer> set = new HashSet();
+
+    for (int num : nums) {
+      if (!set.contains(num)) {
+        set.add(num);
+        sumOfSet += num;
+      }
+      sumOfNums += num;
+    }
+    return 2 * sumOfSet - sumOfNums;
+  }
 }
