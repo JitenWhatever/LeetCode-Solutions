bb4cfbc9d81d4c6bead3a4a5df088932f3c07c6a
jitenwhatever <jitendra1551k@gmail.com>
2021-09-07 21:31:07 +0530

    bit manipulation

--- Medium/Single Number II.java
+++ Medium/Single Number II.java
@@ -1,20 +1,26 @@
 /*
-Given a non-empty array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+Given an integer array nums where every element appears three times except for one, which appears exactly once. 
+Find the single element and return it.
 
-Note:
+You must implement a solution with a linear runtime complexity and use only constant extra space.
 
-Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ 
 
 Example 1:
 
-Input: [2,2,3,2]
+Input: nums = [2,2,3,2]
 Output: 3
-
 Example 2:
 
-Input: [0,1,0,1,0,1,99]
+Input: nums = [0,1,0,1,0,1,99]
 Output: 99
+ 
 
+Constraints:
+
+1 <= nums.length <= 3 * 10^4
+-2^31 <= nums[i] <= 2^31 - 1
+Each element in nums appears exactly three times except for one element which appears once.
 */
 
 class Solution {
@@ -39,4 +45,55 @@ class Solution {
         
         return res;
     }
+}
+
+class Solution {
+  public int singleNumber(int[] nums) {
+    Set<Long> set = new HashSet<>();
+    long sumSet = 0, sumArray = 0;
+    for(int n : nums) {
+      sumArray += n;
+      set.add((long)n);
+    }
+    for(Long s : set) sumSet += s;
+    return (int)((3 * sumSet - sumArray) / 2);
+  }
+}
+
+
+class Solution {
+  public int singleNumber(int[] nums) {
+    HashMap<Integer, Integer> hashmap = new HashMap<>();
+    for (int num : nums)
+      hashmap.put(num, hashmap.getOrDefault(num, 0) + 1);
+
+    for (int k : hashmap.keySet())
+      if (hashmap.get(k) == 1) return k;
+    return -1;
+  }
+}
+
+
+class Solution {
+  public int singleNumber(int[] nums) {
+    int seenOnce = 0, seenTwice = 0;
+
+    for (int num : nums) {
+      // first appearence: 
+      // add num to seen_once 
+      // don't add to seen_twice because of presence in seen_once
+
+      // second appearance: 
+      // remove num from seen_once 
+      // add num to seen_twice
+
+      // third appearance: 
+      // don't add to seen_once because of presence in seen_twice
+      // remove num from seen_twice
+      seenOnce = ~seenTwice & (seenOnce ^ num);
+      seenTwice = ~seenOnce & (seenTwice ^ num);
+    }
+
+    return seenOnce;
+  }
 }
