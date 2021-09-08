371247d88929bdd957ab6789499b06505d3bbbd5
jitenwhatever <jitendra1551k@gmail.com>
2021-09-09 00:20:01 +0530

    bit manipulation

--- Medium/Single Number III.java
+++ Medium/Single Number III.java
@@ -1,19 +1,57 @@
 /*
-Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. 
-Find the two elements that appear only once.
+Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. 
+Find the two elements that appear only once. You can return the answer in any order.
 
-Example:
+You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
 
-Input:  [1,2,1,3,2,5]
+ 
+
+Example 1:
+
+Input: nums = [1,2,1,3,2,5]
 Output: [3,5]
+Explanation:  [5, 3] is also a valid answer.
+Example 2:
 
-Note:
+Input: nums = [-1,0]
+Output: [-1,0]
+Example 3:
 
-    The order of the result is not important. So in the above example, [5, 3] is also correct.
-    Your algorithm should run in linear runtime complexity. 
-    Could you implement it using only constant space complexity?
+Input: nums = [0,1]
+Output: [1,0]
+ 
+
+Constraints:
+
+2 <= nums.length <= 3 * 10^4
+-2^31 <= nums[i] <= 2^31 - 1
+Each integer in nums will appear twice, only two integers will appear once.
 
 */
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int[] result = new int[2];
+        
+        Map<Integer, Integer> numberToCountMap = new HashMap<>();
+        
+        for (int num : nums) {
+            numberToCountMap.put(num, numberToCountMap.getOrDefault(num, 0) + 1);
+        }
+        
+        int index = 0;
+        for (Map.Entry<Integer, Integer> entry : numberToCountMap.entrySet()) {
+            if (entry.getValue() == 1) {
+                result[index++] = entry.getKey();
+            }
+            if (index == 2) {
+                break;
+            }
+        }
+        
+        return result;
+    }
+}
 
 class Solution {
     public int[] singleNumber(int[] nums) {
@@ -41,4 +79,21 @@ class Solution {
         
         return res;
     }
+}
+
+class Solution {
+  public int[] singleNumber(int[] nums) {
+    // difference between two numbers (x and y) which were seen only once
+    int bitmask = 0;
+    for (int num : nums) bitmask ^= num;
+
+    // rightmost 1-bit diff between x and y
+    int diff = bitmask & (-bitmask);
+
+    int x = 0;
+    // bitmask which will contain only x
+    for (int num : nums) if ((num & diff) != 0) x ^= num;
+
+    return new int[]{x, bitmask^x};
+  }
 }
