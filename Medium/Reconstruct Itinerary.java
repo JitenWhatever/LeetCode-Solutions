075fffd8056f6364c84e6a627986f2a7cdcfe3be
jitenwhatever <jitendra1551k@gmail.com>
2021-09-10 19:25:24 +0530

    dfs

--- Medium/Reconstruct Itinerary.java
+++ Medium/Reconstruct Itinerary.java
@@ -1,13 +1,13 @@
 /*
-Given a list of airline tickets represented by pairs of departure and arrival airports [from, to], reconstruct the itinerary in order. 
-All of the tickets belong to a man who departs from JFK. Thus, the itinerary must begin with JFK.
+You are given a list of airline tickets where tickets[i] = [fromi, toi] represent the departure and the arrival airports of one flight. 
+Reconstruct the itinerary in order and return it.
 
-Note:
+All of the tickets belong to a man who departs from "JFK", thus, the itinerary must begin with "JFK". 
+If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string.
 
-If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string. For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
-All airports are represented by three capital letters (IATA code).
-You may assume all tickets form at least one valid itinerary.
-One must use all the tickets once and only once.
+For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+You may assume all tickets form at least one valid itinerary. You must use all the tickets once and only once.
+
 Example 1:
 
 Input: [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
@@ -18,32 +18,54 @@ Input: [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
 Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
 Explanation: Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"].
              But it is larger in lexical order.
+
+
+Constraints:
+
+1 <= tickets.length <= 300
+tickets[i].length == 2
+fromi.length == 3
+toi.length == 3
+fromi and toi consist of uppercase English letters.
+fromi != toi
 */
 
 class Solution {
     
     private List<String> result = new ArrayList<>();
-    private HashMap<String, PriorityQueue<String>> adj = new HashMap<>();
+    private HashMap<String, PriorityQueue<String>> graph = new HashMap<>();
     private void dfs(String src) {
-        PriorityQueue<String> Q = adj.get (src);
+        PriorityQueue<String> Q = graph.get(src);
         
-        while (Q != null && !Q.isEmpty ()) {
-			dfs(Q.poll ());
+        while (Q != null && !Q.isEmpty()) {
+			dfs(Q.poll());
 		}
 		
-		result.add (0, src);
+		result.add(0, src);
     }
     
-    
     public List<String> findItinerary(List<List<String>> tickets) {
-        
         for(List<String> ticket : tickets) {
-            adj.put (ticket.get (0), adj.getOrDefault (ticket.get (0), new PriorityQueue<>()));
-			adj.get (ticket.get (0)).add (ticket.get (1));
+            graph.computeIfAbsent(ticket.get(0), key -> new PriorityQueue<>()).add(ticket.get(1));
         }
         
         dfs("JFK");
         
         return result;
     }
+}
+
+public List<String> findItinerary(String[][] tickets) {
+    Map<String, PriorityQueue<String>> targets = new HashMap<>();
+    for (String[] ticket : tickets)
+        targets.computeIfAbsent(ticket[0], k -> new PriorityQueue()).add(ticket[1]);
+    List<String> route = new LinkedList();
+    Stack<String> stack = new Stack<>();
+    stack.push("JFK");
+    while (!stack.empty()) {
+        while (targets.containsKey(stack.peek()) && !targets.get(stack.peek()).isEmpty())
+            stack.push(targets.get(stack.peek()).poll());
+        route.add(0, stack.pop());
+    }
+    return route;
 }
