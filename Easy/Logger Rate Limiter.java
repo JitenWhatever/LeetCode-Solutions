edb4fd5e8adf91dee23a592fe95d5273292939d7
jitenwhatever <jitendra1551k@gmail.com>
2021-09-11 01:44:08 +0530

    sliding window design

--- Easy/Logger Rate Limiter.java
+++ Easy/Logger Rate Limiter.java
@@ -1,46 +1,142 @@
 /*
-Design a logger system that receive stream of messages along with its timestamps, 
-each message should be printed if and only if it is not printed in the last 10 seconds.
+Design a logger system that receives a stream of messages along with their timestamps. 
+Each unique message should only be printed at most every 10 seconds 
+(i.e. a message printed at timestamp t will prevent other identical messages from being printed until timestamp t + 10).
 
-Given a message and a timestamp (in seconds granularity), 
-return true if the message should be printed in the given timestamp, otherwise returns false.
+All messages will come in chronological order. Several messages may arrive at the same timestamp.
 
-It is possible that several messages arrive roughly at the same time.
+Implement the Logger class:
 
-Example:
+Logger() Initializes the logger object.
+bool shouldPrintMessage(int timestamp, string message) Returns true if the message should be printed in the given timestamp, otherwise returns false.
+ 
 
+Example 1:
+
+Input
+["Logger", "shouldPrintMessage", "shouldPrintMessage", "shouldPrintMessage", "shouldPrintMessage", "shouldPrintMessage", "shouldPrintMessage"]
+[[], [1, "foo"], [2, "bar"], [3, "foo"], [8, "bar"], [10, "foo"], [11, "foo"]]
+Output
+[null, true, true, false, false, false, true]
+
+Explanation
 Logger logger = new Logger();
+logger.shouldPrintMessage(1, "foo");  // return true, next allowed timestamp for "foo" is 1 + 10 = 11
+logger.shouldPrintMessage(2, "bar");  // return true, next allowed timestamp for "bar" is 2 + 10 = 12
+logger.shouldPrintMessage(3, "foo");  // 3 < 11, return false
+logger.shouldPrintMessage(8, "bar");  // 8 < 12, return false
+logger.shouldPrintMessage(10, "foo"); // 10 < 11, return false
+logger.shouldPrintMessage(11, "foo"); // 11 >= 11, return true, next allowed timestamp for "foo" is 11 + 10 = 21
+ 
 
-// logging string "foo" at timestamp 1
-logger.shouldPrintMessage(1, "foo"); returns true; 
+Constraints:
 
-// logging string "bar" at timestamp 2
-logger.shouldPrintMessage(2,"bar"); returns true;
-
-// logging string "foo" at timestamp 3
-logger.shouldPrintMessage(3,"foo"); returns false;
-
-// logging string "bar" at timestamp 8
-logger.shouldPrintMessage(8,"bar"); returns false;
-
-// logging string "foo" at timestamp 10
-logger.shouldPrintMessage(10,"foo"); returns false;
-
-// logging string "foo" at timestamp 11
-logger.shouldPrintMessage(11,"foo"); returns true;
+0 <= timestamp <= 109
+Every timestamp will be passed in non-decreasing order (chronological order).
+1 <= message.length <= 30
+At most 104 calls will be made to shouldPrintMessage.
 */
 
 class Logger {
-    private HashMap<String, Integer> logs = new HashMap<>();
-    public Logger() {}     /** Initialize your data structure here. */
 
+    /** Initialize your data structure here. */
+    private Map<String, Integer> logRepo;
+    public Logger() {
+        this.logRepo = new HashMap<>();
+    }
+    
     /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
         If this method returns false, the message will not be printed.
         The timestamp is in seconds granularity. */
     public boolean shouldPrintMessage(int timestamp, String message) {
-        if (timestamp < logs.getOrDefault(message, 0)) return false;
-
-        logs.put(message, timestamp + 10);
+        if (timestamp < this.logRepo.getOrDefault(message, 0)) {
+            return false;
+        }
+        
+        this.logRepo.put(message, timestamp + 10);
+        
         return true;
     }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
+
+
+ class Pair<U, V> {
+  public U first;
+  public V second;
+
+  public Pair(U first, V second) {
+    this.first = first;
+    this.second = second;
+  }
+}
+
+class Logger {
+  private LinkedList<Pair<String, Integer>> msgQueue;
+  private HashSet<String> msgSet;
+
+  /** Initialize your data structure here. */
+  public Logger() {
+    msgQueue = new LinkedList<Pair<String, Integer>>();
+    msgSet = new HashSet<String>();
+  }
+
+  /**
+   * Returns true if the message should be printed in the given timestamp, otherwise returns false.
+   */
+  public boolean shouldPrintMessage(int timestamp, String message) {
+
+    // clean up.
+    while (msgQueue.size() > 0) {
+      Pair<String, Integer> head = msgQueue.getFirst();
+      if (timestamp - head.second >= 10) {
+        msgQueue.removeFirst();
+        msgSet.remove(head.first);
+      } else
+        break;
+    }
+
+    if (!msgSet.contains(message)) {
+      Pair<String, Integer> newEntry = new Pair<String, Integer>(message, timestamp);
+      msgQueue.addLast(newEntry);
+      msgSet.add(message);
+      return true;
+    } else
+      return false;
+
+  }
+}
+
+
+
+class Logger {
+  private HashMap<String, Integer> msgDict;
+
+  /** Initialize your data structure here. */
+  public Logger() {
+    msgDict = new HashMap<String, Integer>();
+  }
+
+  /**
+   * Returns true if the message should be printed in the given timestamp, otherwise returns false.
+   */
+  public boolean shouldPrintMessage(int timestamp, String message) {
+
+    if (!this.msgDict.containsKey(message)) {
+      this.msgDict.put(message, timestamp);
+      return true;
+    }
+
+    Integer oldTimestamp = this.msgDict.get(message);
+    if (timestamp - oldTimestamp >= 10) {
+      this.msgDict.put(message, timestamp);
+      return true;
+    } else
+      return false;
+  }
 }
