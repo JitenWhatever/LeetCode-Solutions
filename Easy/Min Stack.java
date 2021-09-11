159f756149e45963a30730c22228ba855ecc57fd
jitenwhatever <jitendra1551k@gmail.com>
2021-09-12 02:09:59 +0530

    stacks

--- Easy/Min Stack.java
+++ Easy/Min Stack.java
@@ -1,10 +1,14 @@
 /*
 Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
 
-    push(x) -- Push element x onto stack.
-    pop() -- Removes the element on top of the stack.
-    top() -- Get the top element.
-    getMin() -- Retrieve the minimum element in the stack.
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+ 
 
 Example 1:
 
@@ -24,51 +28,148 @@ minStack.getMin(); // return -3
 minStack.pop();
 minStack.top();    // return 0
 minStack.getMin(); // return -2
+ 
 
 Constraints:
-    Methods pop, top and getMin operations will always be called on non-empty stacks.
+
+-2^31 <= val <= 2^31 - 1
+Methods pop, top and getMin operations will always be called on non-empty stacks.
+At most 3 * 10^4 calls will be made to push, pop, top, and getMin.
 */
 
 class MinStack {
 
     /** initialize your data structure here. */
-    Stack<Integer> stack, minStack;
+    Stack<Integer> stck, minStack;
     public MinStack() {
-        stack = new Stack<Integer>();
-        minStack = new Stack<Integer>();
+        this.stck = new Stack<>();
+        this.minStack = new Stack<>();
     }
     
-    public void push(int x) {
-        
-        if(minStack.isEmpty() || x <= minStack.peek()) {
-            minStack.push(x);
+    public void push(int val) {
+        if (this.minStack.isEmpty() || val <= this.minStack.peek()) {
+            this.minStack.push(val);
         }
         
-        stack.push(x);
+        this.stck.push(val);
     }
     
     public void pop() {
-        if(stack.peek().equals(minStack.peek())) {
-            minStack.pop();
+        if (this.stck.peek().equals(this.minStack.peek())) {
+            this.minStack.pop();
         }
         
-        stack.pop();
+        stck.pop();
     }
     
     public int top() {
-        return stack.peek();
+        return this.stck.peek();
     }
     
     public int getMin() {
-        return minStack.peek();
+       return  this.minStack.peek();
     }
 }
 
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
- * obj.push(x);
+ * obj.push(val);
  * obj.pop();
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+
+ class MinStack {
+    
+    private Stack<int[]> stack = new Stack<>();
+    
+    public MinStack() { }
+    
+    
+    public void push(int x) {
+        
+        /* If the stack is empty, then the min value
+         * must just be the first value we add. */
+        if (stack.isEmpty()) {
+            stack.push(new int[]{x, x});
+            return;
+        }
+        
+        int currentMin = stack.peek()[1];
+        stack.push(new int[]{x, Math.min(x, currentMin)});
+    }
+    
+    
+    public void pop() {
+        stack.pop();
+    }
+    
+    
+    public int top() {
+        return stack.peek()[0];
+    }
+    
+    
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
+
+
+class MinStack {
+
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<int[]> minStack = new Stack<>();
+    
+    
+    public MinStack() { }
+    
+    
+    public void push(int x) {
+        
+        // We always put the number onto the main stack.
+        stack.push(x);
+        
+        // If the min stack is empty, or this number is smaller than
+        // the top of the min stack, put it on with a count of 1.
+        if (minStack.isEmpty() || x < minStack.peek()[0]) {
+            minStack.push(new int[]{x, 1});
+        }
+        
+        // Else if this number is equal to what's currently at the top
+        // of the min stack, then increment the count at the top by 1.
+        else if (x == minStack.peek()[0]) {
+            minStack.peek()[1]++;
+        }
+    }
+    
+    
+    public void pop() {
+        
+        // If the top of min stack is the same as the top of stack
+        // then we need to decrement the count at the top by 1.
+        if (stack.peek().equals(minStack.peek()[0])) {
+            minStack.peek()[1]--;
+        }
+        
+        // If the count at the top of min stack is now 0, then remove
+        // that value as we're done with it.
+        if (minStack.peek()[1] == 0) {
+            minStack.pop();
+        }
+        
+        // And like before, pop the top of the main stack.
+        stack.pop();
+    }
+    
+    
+    public int top() {
+        return stack.peek();
+    }
+
+    
+    public int getMin() {
+        return minStack.peek()[0];
+    }
+}
