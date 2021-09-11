350984eff88e9429cf86d2cba247cc80e0799a6d
jitenwhatever <jitendra1551k@gmail.com>
2021-09-11 21:54:21 +0530

    design

--- Easy/Design HashSet.java
+++ Easy/Design HashSet.java
@@ -1,29 +1,37 @@
 /*
 Design a HashSet without using any built-in hash table libraries.
 
-To be specific, your design should include these functions:
+Implement MyHashSet class:
 
-add(value): Insert a value into the HashSet. 
-contains(value) : Return whether the value exists in the HashSet or not.
-remove(value): Remove a value in the HashSet. If the value does not exist in the HashSet, do nothing.
+void add(key) Inserts the value key into the HashSet.
+bool contains(key) Returns whether the value key exists in the HashSet or not.
+void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+ 
 
-Example:
+Example 1:
 
-MyHashSet hashSet = new MyHashSet();
-hashSet.add(1);         
-hashSet.add(2);         
-hashSet.contains(1);    // returns true
-hashSet.contains(3);    // returns false (not found)
-hashSet.add(2);          
-hashSet.contains(2);    // returns true
-hashSet.remove(2);          
-hashSet.contains(2);    // returns false (already removed)
+Input
+["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+[[], [1], [2], [1], [3], [2], [2], [2], [2]]
+Output
+[null, null, null, true, false, null, true, null, false]
 
-Note:
+Explanation
+MyHashSet myHashSet = new MyHashSet();
+myHashSet.add(1);      // set = [1]
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(1); // return True
+myHashSet.contains(3); // return False, (not found)
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(2); // return True
+myHashSet.remove(2);   // set = [1]
+myHashSet.contains(2); // return False, (already removed)
+ 
 
-All values will be in the range of [0, 1000000].
-The number of operations will be in the range of [1, 10000].
-Please do not use the built-in HashSet library.
+Constraints:
+
+0 <= key <= 10^6
+At most 104 calls will be made to add, remove, and contains.
 */
 
 class MyHashSet {
@@ -56,6 +64,225 @@ class MyHashSet {
         key = key%32;
         return 1<<key;
     }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
+
+
+ 
+class MyHashSet {
+  private Bucket[] bucketArray;
+  private int keyRange;
+
+  /** Initialize your data structure here. */
+  public MyHashSet() {
+    this.keyRange = 769;
+    this.bucketArray = new Bucket[this.keyRange];
+    for (int i = 0; i < this.keyRange; ++i)
+      this.bucketArray[i] = new Bucket();
+  }
+
+  protected int _hash(int key) {
+    return (key % this.keyRange);
+  }
+
+  public void add(int key) {
+    int bucketIndex = this._hash(key);
+    this.bucketArray[bucketIndex].insert(key);
+  }
+
+  public void remove(int key) {
+    int bucketIndex = this._hash(key);
+    this.bucketArray[bucketIndex].delete(key);
+  }
+
+  /** Returns true if this set contains the specified element */
+  public boolean contains(int key) {
+    int bucketIndex = this._hash(key);
+    return this.bucketArray[bucketIndex].exists(key);
+  }
+}
+
+
+class Bucket {
+  private LinkedList<Integer> container;
+
+  public Bucket() {
+    container = new LinkedList<Integer>();
+  }
+
+  public void insert(Integer key) {
+    int index = this.container.indexOf(key);
+    if (index == -1) {
+      this.container.addFirst(key);
+    }
+  }
+
+  public void delete(Integer key) {
+    this.container.remove(key);
+  }
+
+  public boolean exists(Integer key) {
+    int index = this.container.indexOf(key);
+    return (index != -1);
+  }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
+
+
+ 
+class MyHashSet {
+  private Bucket[] bucketArray;
+  private int keyRange;
+
+  /** Initialize your data structure here. */
+  public MyHashSet() {
+    this.keyRange = 769;
+    this.bucketArray = new Bucket[this.keyRange];
+    for (int i = 0; i < this.keyRange; ++i)
+      this.bucketArray[i] = new Bucket();
+  }
+
+  protected int _hash(int key) {
+    return (key % this.keyRange);
+  }
+
+  public void add(int key) {
+    int bucketIndex = this._hash(key);
+    this.bucketArray[bucketIndex].insert(key);
+  }
+
+  public void remove(int key) {
+    int bucketIndex = this._hash(key);
+    this.bucketArray[bucketIndex].delete(key);
+  }
+
+  /** Returns true if this set contains the specified element */
+  public boolean contains(int key) {
+    int bucketIndex = this._hash(key);
+    return this.bucketArray[bucketIndex].exists(key);
+  }
+}
+
+
+class Bucket {
+  private BSTree tree;
+
+  public Bucket() {
+    tree = new BSTree();
+  }
+
+  public void insert(Integer key) {
+    this.tree.root = this.tree.insertIntoBST(this.tree.root, key);
+  }
+
+  public void delete(Integer key) {
+    this.tree.root = this.tree.deleteNode(this.tree.root, key);
+  }
+
+  public boolean exists(Integer key) {
+    TreeNode node = this.tree.searchBST(this.tree.root, key);
+    return (node != null);
+  }
+}
+
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int x) {
+    val = x;
+  }
+}
+
+class BSTree {
+  TreeNode root = null;
+
+  public TreeNode searchBST(TreeNode root, int val) {
+    if (root == null || val == root.val)
+      return root;
+
+    return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val);
+  }
+
+  public TreeNode insertIntoBST(TreeNode root, int val) {
+    if (root == null)
+      return new TreeNode(val);
+
+    if (val > root.val)
+      // insert into the right subtree
+      root.right = insertIntoBST(root.right, val);
+    else if (val == root.val)
+      // skip the insertion
+      return root;
+    else
+      // insert into the left subtree
+      root.left = insertIntoBST(root.left, val);
+    return root;
+  }
+
+  /*
+   * One step right and then always left
+   */
+  public int successor(TreeNode root) {
+    root = root.right;
+    while (root.left != null)
+      root = root.left;
+    return root.val;
+  }
+
+  /*
+   * One step left and then always right
+   */
+  public int predecessor(TreeNode root) {
+    root = root.left;
+    while (root.right != null)
+      root = root.right;
+    return root.val;
+  }
+
+  public TreeNode deleteNode(TreeNode root, int key) {
+    if (root == null)
+      return null;
+
+    // delete from the right subtree
+    if (key > root.val)
+      root.right = deleteNode(root.right, key);
+    // delete from the left subtree
+    else if (key < root.val)
+      root.left = deleteNode(root.left, key);
+    // delete the current node
+    else {
+      // the node is a leaf
+      if (root.left == null && root.right == null)
+        root = null;
+      // the node is not a leaf and has a right child
+      else if (root.right != null) {
+        root.val = successor(root);
+        root.right = deleteNode(root.right, root.val);
+      }
+      // the node is not a leaf, has no right child, and has a left child
+      else {
+        root.val = predecessor(root);
+        root.left = deleteNode(root.left, root.val);
+      }
+    }
+    return root;
+  }
 }
 
 /**
