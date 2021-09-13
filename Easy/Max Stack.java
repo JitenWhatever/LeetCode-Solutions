184af1bd492e7e1c53ccf86e8729b58891dcadd5
JitenWhatever <jitendra1551@gmail.com>
2021-09-13 23:08:18 +0530

    ddl + treemap + two stacks

--- Easy/Max Stack.java
+++ Easy/Max Stack.java
@@ -1,29 +1,46 @@
 /*
-Design a max stack that supports push, pop, top, peekMax and popMax.
+Design a max stack data structure that supports the stack operations and supports finding the stack's maximum element.
 
-    push(x) -- Push element x onto stack.
-    pop() -- Remove the element on top of the stack and return it.
-    top() -- Get the element on the top.
-    peekMax() -- Retrieve the maximum element in the stack.
-    popMax() -- Retrieve the maximum element in the stack, and remove it. If you find more than one maximum elements, only remove the top-most one.
+Implement the MaxStack class:
+
+MaxStack() Initializes the stack object.
+void push(int x) Pushes element x onto the stack.
+int pop() Removes the element on top of the stack and returns it.
+int top() Gets the element on the top of the stack without removing it.
+int peekMax() Retrieves the maximum element in the stack without removing it.
+int popMax() Retrieves the maximum element in the stack and removes it. 
+If there is more than one maximum element, only remove the top-most one.
+ 
 
 Example 1:
 
-MaxStack stack = new MaxStack();
-stack.push(5); 
-stack.push(1);
-stack.push(5);
-stack.top(); -> 5
-stack.popMax(); -> 5
-stack.top(); -> 1
-stack.peekMax(); -> 5
-stack.pop(); -> 1
-stack.top(); -> 5
+Input
+["MaxStack", "push", "push", "push", "top", "popMax", "top", "peekMax", "pop", "top"]
+[[], [5], [1], [5], [], [], [], [], [], []]
+Output
+[null, null, null, null, 5, 5, 1, 5, 1, 5]
 
-Note:
-    -1e7 <= x <= 1e7
-    Number of operations won't exceed 10000.
-    The last four operations won't be called when stack is empty.
+Explanation
+MaxStack stk = new MaxStack();
+stk.push(5);   // [5] the top of the stack and the maximum number is 5.
+stk.push(1);   // [5, 1] the top of the stack is 1, but the maximum is 5.
+stk.push(5);   // [5, 1, 5] the top of the stack is 5, which is also the maximum, because it is the top most one.
+stk.top();     // return 5, [5, 1, 5] the stack did not change.
+stk.popMax();  // return 5, [5, 1] the stack is changed now, and the top is different from the max.
+stk.top();     // return 1, [5, 1] the stack did not change.
+stk.peekMax(); // return 5, [5, 1] the stack did not change.
+stk.pop();     // return 1, [5] the top of the stack and the max element is now 5.
+stk.top();     // return 5, [5] the stack did not change.
+ 
+
+Constraints:
+
+-10^7 <= x <= 10^7
+At most 104 calls will be made to push, pop, top, peekMax, and popMax.
+There will be at least one element in the stack when pop, top, peekMax, or popMax is called.
+ 
+
+Follow up: Could you come up with a solution that supports O(1) for each top call and O(logn) for each other call? 
 */
 
 class MaxStack {
@@ -66,4 +83,144 @@ class MaxStack {
         while (!buffer.isEmpty()) push(buffer.pop());
         return max;
     }
+}
+class MaxStack {
+
+    /** initialize your data structure here. */
+    private Stack<Integer> stck, maxStack;
+    public MaxStack() {
+        this.stck = new Stack<>();
+        this.maxStack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        if (this.maxStack.isEmpty() || x >= this.maxStack.peek()) {
+            this.maxStack.push(x);
+        } else {
+            this.maxStack.push(this.maxStack.peek());
+        }
+        
+        this.stck.push(x);
+    }
+    
+    public int pop() {
+        this.maxStack.pop();
+        return this.stck.pop();
+    }
+    
+    public int top() {
+        return this.stck.peek();
+    }
+    
+    public int peekMax() {
+        return this.maxStack.peek();
+    }
+    
+    public int popMax() {
+        int max = peekMax();
+        Stack<Integer> buffer = new Stack();
+        while (top() != max) {
+            buffer.push(pop());
+        }
+        
+        pop();
+
+        while (!buffer.isEmpty()){ 
+            push(buffer.pop());                          
+        }
+        return max;
+    }
+}
+
+/**
+ * Your MaxStack object will be instantiated and called as such:
+ * MaxStack obj = new MaxStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.peekMax();
+ * int param_5 = obj.popMax();
+ */
+
+
+
+ class MaxStack {
+    TreeMap<Integer, List<Node>> map;
+    DoubleLinkedList dll;
+
+    public MaxStack() {
+        map = new TreeMap();
+        dll = new DoubleLinkedList();
+    }
+
+    public void push(int x) {
+        Node node = dll.add(x);
+        if(!map.containsKey(x))
+            map.put(x, new ArrayList<Node>());
+        map.get(x).add(node);
+    }
+
+    public int pop() {
+        int val = dll.pop();
+        List<Node> L = map.get(val);
+        L.remove(L.size() - 1);
+        if (L.isEmpty()) map.remove(val);
+        return val;
+    }
+
+    public int top() {
+        return dll.peek();
+    }
+
+    public int peekMax() {
+        return map.lastKey();
+    }
+
+    public int popMax() {
+        int max = peekMax();
+        List<Node> L = map.get(max);
+        Node node = L.remove(L.size() - 1);
+        dll.unlink(node);
+        if (L.isEmpty()) map.remove(max);
+        return max;
+    }
+}
+
+class DoubleLinkedList {
+    Node head, tail;
+
+    public DoubleLinkedList() {
+        head = new Node(0);
+        tail = new Node(0);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public Node add(int val) {
+        Node x = new Node(val);
+        x.next = tail;
+        x.prev = tail.prev;
+        tail.prev = tail.prev.next = x;
+        return x;
+    }
+
+    public int pop() {
+        return unlink(tail.prev).val;
+    }
+
+    public int peek() {
+        return tail.prev.val;
+    }
+
+    public Node unlink(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        return node;
+    }
+}
+
+class Node {
+    int val;
+    Node prev, next;
+    public Node(int v) {val = v;}
 }
