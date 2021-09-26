51cea8e5c202ece289d142cb037d57e31147ed36
JitenWhatever <jitendra1551k@gmail.com>
2021-09-26 21:38:50 +0530

    list + dequeue + cal

--- Easy/Moving Average from Data Stream.java
+++ Easy/Moving Average from Data Stream.java
@@ -1,31 +1,119 @@
 /*
-Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+Given a stream of integers and a window size, 
+calculate the moving average of all integers in the sliding window.
 
-Example:
-MovingAverage m = new MovingAverage(3);
-m.next(1) = 1
-m.next(10) = (1 + 10) / 2
-m.next(3) = (1 + 10 + 3) / 3
-m.next(5) = (10 + 3 + 5) / 3
+Implement the MovingAverage class:
+
+MovingAverage(int size) Initializes the object with the size of the window size.
+double next(int val) Returns the moving average of the last size values of the stream.
+ 
+
+Example 1:
+
+Input
+["MovingAverage", "next", "next", "next", "next"]
+[[3], [1], [10], [3], [5]]
+Output
+[null, 1.0, 5.5, 4.66667, 6.0]
+
+Explanation
+MovingAverage movingAverage = new MovingAverage(3);
+movingAverage.next(1); // return 1.0 = 1 / 1
+movingAverage.next(10); // return 5.5 = (1 + 10) / 2
+movingAverage.next(3); // return 4.66667 = (1 + 10 + 3) / 3
+movingAverage.next(5); // return 6.0 = (10 + 3 + 5) / 3
+ 
+
+Constraints:
+
+1 <= size <= 1000
+-10^5 <= val <= 10^5
+At most 104 calls will be made to next.
 */
 
 class MovingAverage {
-    private Queue<integer> window;
-    private int sum = 0;
-    private int size;
 
-    public MovingAverage(int s) {
-        window = new LinkedList<>();
-        size = s;
+    private Queue<Integer> window;
+    private int size, sum;
+    public MovingAverage(int size) {
+        this.window = new LinkedList<>();
+        this.size = size;
+        this.sum = 0;
     }
-
+    
     public double next(int val) {
-        if(window.size() == size){
-            sum -= window.poll();
+        if (this.window.size() == this.size) {
+            this.sum -= this.window.poll();
         }
-
-        window.add(val);
-        sum += val;
-        return sum/window.size();
+        
+        this.window.add(val);
+        
+        this.sum += val;
+        
+        return (double)this.sum/this.window.size(); 
     }
-};
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
+
+ class MovingAverage {
+  int size;
+  List queue = new ArrayList<Integer>();
+  public MovingAverage(int size) {
+    this.size = size;
+  }
+
+  public double next(int val) {
+    queue.add(val);
+    // calculate the sum of the moving window
+    int windowSum = 0;
+    for(int i = Math.max(0, queue.size() - size); i < queue.size(); ++i)
+      windowSum += (int)queue.get(i);
+
+    return windowSum * 1.0 / Math.min(queue.size(), size);
+  }
+}
+
+class MovingAverage {
+  int size, windowSum = 0, count = 0;
+  Deque queue = new ArrayDeque<Integer>();
+
+  public MovingAverage(int size) {
+    this.size = size;
+  }
+
+  public double next(int val) {
+    ++count;
+    // calculate the new sum by shifting the window
+    queue.add(val);
+    int tail = count > size ? (int)queue.poll() : 0;
+
+    windowSum = windowSum - tail + val;
+
+    return windowSum * 1.0 / Math.min(size, count);
+  }
+}
+
+class MovingAverage {
+  int size, head = 0, windowSum = 0, count = 0;
+  int[] queue;
+  public MovingAverage(int size) {
+    this.size = size;
+    queue = new int[size];
+  }
+
+  public double next(int val) {
+    ++count;
+    // calculate the new sum by shifting the window
+    int tail = (head + 1) % size;
+    windowSum = windowSum - queue[tail] + val;
+    // move on to the next head
+    head = (head + 1) % size;
+    queue[head] = val;
+    return windowSum * 1.0 / Math.min(size, count);
+  }
+}
