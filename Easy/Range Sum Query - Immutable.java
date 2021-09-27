ab92d494e1c82114763c04624d749274e109a264
JitenWhatever <jitendra1551k@gmail.com>
2021-09-27 22:11:01 +0530

    prefix sum

--- Easy/Range Sum Query - Immutable.java
+++ Easy/Range Sum Query - Immutable.java
@@ -1,5 +1,35 @@
 /*
-Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+Given an integer array nums, handle multiple queries of the following type:
+
+Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+Implement the NumArray class:
+
+NumArray(int[] nums) Initializes the object with the integer array nums.
+int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right 
+inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+ 
+
+Example 1:
+
+Input
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Output
+[null, 1, -1, -3]
+
+Explanation
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-105 <= nums[i] <= 105
+0 <= left <= right < nums.length
+At most 104 calls will be made to sumRangeGiven an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
 
 Example:
 
@@ -21,24 +51,57 @@ There are many calls to sumRange function.
 
 class NumArray {
 
+    int[] nums;
     public NumArray(int[] nums) {
-        prefixSum = new int[nums.length + 1];
-        
-        
-        for(int index = 0; index < nums.length; ++index) {
-          prefixSum[index + 1] += prefixSum[index] + nums[index]; 
+        int len = nums.length;
+        this.nums = nums;
+       for (int index = 1; index < len; ++index) {
+           nums[index] += nums[index - 1];
+       }        
+    }
+    
+    public int sumRange(int left, int right) {
+        --left;
+        if (left < 0) {
+            return this.nums[right];
         }
+        
+        return this.nums[right] - this.nums[left];
     }
-    
-    public int sumRange(int i, int j) {
-        return prefixSum[j+1] - prefixSum[i];
-    }
-    
-    private int[] prefixSum;
 }
 
 /**
  * Your NumArray object will be instantiated and called as such:
  * NumArray obj = new NumArray(nums);
- * int param_1 = obj.sumRange(i,j);
+ * int param_1 = obj.sumRange(left,right);
  */
+
+ private int[] data;
+
+public NumArray(int[] nums) {
+    data = nums;
+}
+
+public int sumRange(int i, int j) {
+    int sum = 0;
+    for (int k = i; k <= j; k++) {
+        sum += data[k];
+    }
+    return sum;
+}
+
+private Map<Pair<Integer, Integer>, Integer> map = new HashMap<>();
+
+public NumArray(int[] nums) {
+    for (int i = 0; i < nums.length; i++) {
+        int sum = 0;
+        for (int j = i; j < nums.length; j++) {
+            sum += nums[j];
+            map.put(Pair.create(i, j), sum);
+        }
+    }
+}
+
+public int sumRange(int i, int j) {
+    return map.get(Pair.create(i, j));
+}
