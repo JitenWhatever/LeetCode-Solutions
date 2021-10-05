2c92775ac6d3609b19b848da9569e8cc1a094bcc
JitenWhatever <jitendra1551k@gmail.com>
2021-10-06 00:18:21 +0530

    simulate

--- Easy/Happy Number.java
+++ Easy/Happy Number.java
@@ -1,46 +1,126 @@
 /*
-Write an algorithm to determine if a number n is "happy".
-A happy number is a number defined by the following process: 
-Starting with any positive integer, replace the number by the sum of the squares of its digits, 
-and repeat the process until the number equals 1 (where it will stay), 
-or it loops endlessly in a cycle which does not include 1. 
-Those numbers for which this process ends in 1 are happy numbers.
-Return True if n is a happy number, and False if not.
+Write an algorithm to determine if a number n is happy.
 
-Example: 
+A happy number is a number defined by the following process:
 
-Input: 19
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or 
+it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+
+ 
+
+Example 1:
+
+Input: n = 19
 Output: true
-Explanation: 
+Explanation:
 12 + 92 = 82
 82 + 22 = 68
 62 + 82 = 100
 12 + 02 + 02 = 1
+Example 2:
+
+Input: n = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= n <= 2^31 - 1
 */
 
 class Solution {
     public boolean isHappy(int n) {
+        Set<Integer> alreadySeen = new HashSet<>();
         
-        Set<Integer> seen = new HashSet<Integer>();
-        
-        while(n != 1){
-            n = getDigitsSum(n);
-            if(seen.contains(n)){
-                return false;
-            }
-            seen.add(n);
+        while (n != 1) {
+           if (!alreadySeen.add(n)) {
+               return false;
+           }
+           n = getSquareSumOfDigits(n);
         }
         
         return true;
     }
     
-    private Integer getDigitsSum(Integer num){
-        Integer sum = 0;
-        while(num != 0){
-            sum += ((num % 10) * (num % 10));
-            num /= 10;
+    private int getSquareSumOfDigits(int n) {
+        int sum = 0;
+        
+        while (n > 0) {
+            int digit = n % 10;
+            n /= 10;
+            sum += (digit * digit);
         }
         
         return sum;
+    }
+}
+
+class Solution {
+
+     public int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+    public boolean isHappy(int n) {
+        int slowRunner = n;
+        int fastRunner = getNext(n);
+        while (fastRunner != 1 && slowRunner != fastRunner) {
+            slowRunner = getNext(slowRunner);
+            fastRunner = getNext(getNext(fastRunner));
+        }
+        return fastRunner == 1;
+    }
+}
+
+class Solution {
+
+    private static Set<Integer> cycleMembers =
+        new HashSet<>(Arrays.asList(4, 16, 37, 58, 89, 145, 42, 20));
+
+    public int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+
+    public boolean isHappy(int n) {
+        while (n != 1 && !cycleMembers.contains(n)) {
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+}
+
+class Solution {
+    
+    public int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+    
+    public boolean isHappy(int n) {
+        while (n != 1 && n != 4) {
+            n = getNext(n);
+        }
+        return n == 1;
     }
 }
