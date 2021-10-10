bcf57eb1a37d31b4ef2c6ea8bab36c3599db7cb9
JitenWhatever <jitendra1551k@gmail.com>
2021-10-10 20:59:29 +0530

    dp + bfs + combination sum

--- Medium/Perfect Squares.java
+++ Medium/Perfect Squares.java
@@ -1,46 +1,53 @@
 /*
-Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+Given an integer n, return the least number of perfect square numbers that sum to n.
+
+A perfect square is an integer that is the square of an integer; 
+in other words, it is the product of some integer with itself. 
+For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
 
 Example 1:
 
 Input: n = 12
-Output: 3 
+Output: 3
 Explanation: 12 = 4 + 4 + 4.
-
 Example 2:
 
 Input: n = 13
 Output: 2
-Explanation: 13 = 4 + 9
+Explanation: 13 = 4 + 9.
+ 
+
+Constraints:
+
+1 <= n <= 10^4
 */
 
 class Solution {
     public int numSquares(int n) {
         
-        if(n < 4) {
+        if (n < 4) {
             return n;
         }
         
-        List<Integer> coins = new ArrayList<>();
+        int[] dp = new int[n + 1];
         
-        for(int num = 1; num * num <= n; ++num) {
-            coins.add(num * num);
+        int[] coins = new int[n + 1];
+        
+        for (int num = 0; num <= n; ++num) {
+            coins[num] = num*num;
         }
         
-        
-        int[] dp = new int[n + 1];
-        for(int num = 0; num < 4; ++num) {
+        for (int num = 0; num < 4; ++num) {
             dp[num] = num;
         }
         
         dp[4] = 1;
         
-        for(int amount = 5; amount <= n; ++amount) {
+        for (int amount = 5; amount <= n; ++amount) {
             dp[amount] = amount;
-            for(int coin : coins) {
-                if(amount >= coin) {
-                    dp[amount] = Math.min(dp[amount], dp[amount - coin] + 1);
-                }
+            for (int coin : coins) {
+                if (amount >= coin)
+                dp[amount] = Math.min(dp[amount], dp[amount - coin] + 1);
             }
         }
         
