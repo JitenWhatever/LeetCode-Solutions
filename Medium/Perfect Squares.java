ab72d1e03cfd7eadede12adb7aa036014cbd8ee1
JitenWhatever <jitendra1551k@gmail.com>
2021-10-10 20:59:47 +0530

    dp + bfs + combination sum

--- Medium/Perfect Squares.java
+++ Medium/Perfect Squares.java
@@ -54,3 +54,33 @@ class Solution {
         return dp[n];
     }
 }
+
+
+public int numSquares(int n) {
+    Queue<Integer> q = new LinkedList<>();
+    Set<Integer> visited = new HashSet<>();
+    q.offer(0);
+    visited.add(0);
+    int depth = 0;
+    while(!q.isEmpty()) {
+        int size = q.size();
+        depth++;
+        while(size-- > 0) {
+            int u = q.poll();
+            for(int i = 1; i*i <= n; i++) {
+                int v = u+i*i;
+                if(v == n) {
+                    return depth;
+                }
+                if(v > n) {
+                    break;
+                }
+                if(!visited.contains(v)) {
+                    q.offer(v);
+                    visited.add(v);
+                }
+            }
+        }
+    }
+    return depth;
+}
