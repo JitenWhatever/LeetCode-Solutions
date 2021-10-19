a7a1a3c7ccb1a66b78df01105c79a97cc2122164
JitenWhatever <jitendra1551k@gmail.com>
2021-10-20 00:08:19 +0530

    heap

--- Medium/Meeting Rooms ii.java
+++ Medium/Meeting Rooms ii.java
@@ -1,54 +1,128 @@
 /*
-Given an array of meeting time intervals consisting of start and end times [s1, e1], [s2, e2], ... , find the minimum number of conference rooms required.
+Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], 
+return the minimum number of conference rooms required.
 
-For example,
-Given [ [0, 30], [5, 10], [15, 20] ],
-return 2
+Example 1:
 
-For example,
-Given [ [7, 10], [2, 4]],
-return 1
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: 2
+Example 2:
+
+Input: intervals = [[7,10],[2,4]]
+Output: 1
+ 
+
+Constraints:
+
+1 <= intervals.length <= 10^4
+0 <= starti < endi <= 10^6
 */
 
-/**
-Definition for an interval.
-public class Interval {
-    int start;
-    int end;
-    Interval() {
-        start = 0;
-        end = 0;
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        
+    // Check for the base case. If there are no intervals, return 0
+    if (intervals.length == 0) {
+      return 0;
     }
 
-    Interval(int s, int e) {
-        start = s;
-        end = e;
+    // Min heap
+    PriorityQueue<Integer> allocator =
+        new PriorityQueue<Integer>(
+            intervals.length,
+            new Comparator<Integer>() {
+              public int compare(Integer a, Integer b) {
+                return a - b;
+              }
+            });
+
+    // Sort the intervals by start time
+    Arrays.sort(
+        intervals,
+        new Comparator<int[]>() {
+          public int compare(final int[] a, final int[] b) {
+            return a[0] - b[0];
+          }
+        });
+
+    // Add the first meeting
+    allocator.add(intervals[0][1]);
+
+    // Iterate over remaining intervals
+    for (int i = 1; i < intervals.length; i++) {
+
+      // If the room due to free up the earliest is free, assign that room to this meeting.
+      if (intervals[i][0] >= allocator.peek()) {
+        allocator.poll();
+      }
+
+      // If a new room is to be assigned, then also we add to the heap,
+      // If an old room is allocated, then also we have to add to the heap with updated end time.
+      allocator.add(intervals[i][1]);
     }
+
+    // The size of the heap tells us the minimum rooms required for all the meetings.
+    return allocator.size();
+  }
 }
-*/
 
-public int minMeetingRooms(int[][] intervals) {
-    
-    if(intervals == null || intervals.length == 0) {
-        return 0;
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        
+    // Check for the base case. If there are no intervals, return 0
+    if (intervals.length == 0) {
+      return 0;
     }
 
-    Arrays.sort(intervals, (a, b) -> a.start - b.start);
-    PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> a.end - b.end));
-    minHeap.add(interval[0]);
-    
-    for(int index = 1; index < intervals.length; ++index) {
-        Interval current = intervals[index];
-        Interval early = minHeap.remove();
+    Integer[] start = new Integer[intervals.length];
+    Integer[] end = new Integer[intervals.length];
 
-        if(current.start >= early.end) {
-            early.end = current.end;
-        }
-        else {
-            minHeap.add(current);
-        }
-
-        minHeap.add(early);
+    for (int i = 0; i < intervals.length; i++) {
+      start[i] = intervals[i][0];
+      end[i] = intervals[i][1];
     }
-    return minHeap.size();
+
+    // Sort the intervals by end time
+    Arrays.sort(
+        end,
+        new Comparator<Integer>() {
+          public int compare(Integer a, Integer b) {
+            return a - b;
+          }
+        });
+
+    // Sort the intervals by start time
+    Arrays.sort(
+        start,
+        new Comparator<Integer>() {
+          public int compare(Integer a, Integer b) {
+            return a - b;
+          }
+        });
+
+    // The two pointers in the algorithm: e_ptr and s_ptr.
+    int startPointer = 0, endPointer = 0;
+
+    // Variables to keep track of maximum number of rooms used.
+    int usedRooms = 0;
+
+    // Iterate over intervals.
+    while (startPointer < intervals.length) {
+
+      // If there is a meeting that has ended by the time the meeting at `start_pointer` starts
+      if (start[startPointer] >= end[endPointer]) {
+        usedRooms -= 1;
+        endPointer += 1;
+      }
+
+      // We do this irrespective of whether a room frees up or not.
+      // If a room got free, then this used_rooms += 1 wouldn't have any effect. used_rooms would
+      // remain the same in that case. If no room was free, then this would increase used_rooms
+      usedRooms += 1;
+      startPointer += 1;
+
+    }
+
+    return usedRooms;
+  }
 }
