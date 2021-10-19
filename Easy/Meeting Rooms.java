b7d02ef66f869f9f690e2ef3a1eb29f8f3aaa4e8
JitenWhatever <jitendra1551k@gmail.com>
2021-10-19 23:54:20 +0530

    sort

--- Easy/Meeting Rooms.java
+++ Easy/Meeting Rooms.java
@@ -1,41 +1,37 @@
 /*
-Given an array of meeting time intervals consisting of start and end times [s1, e1], [s2, e2], ... , determine if a person could attend all meetings.
+Given an array of meeting time intervals where intervals[i] = [starti, endi], 
+determine if a person could attend all meetings.
 
-For example,
-Given [ [0, 30], [5, 10], [15, 20] ],
-return false
+Example 1:
 
-For example,
-Given [ [7, 10], [2, 4]],
-return true
-*/
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: false
+Example 2:
 
-/**
-Definition for an interval.
-public class Interval {
-    int start;
-    int end;
-    Interval() {
-        start = 0;
-        end = 0;
-    }
-
-    Interval(int s, int e) {
-        start = s;
-        end = e;
-    }
-}
-*/
-public boolean canAttendMeetings(Interval[] intervals) {
-    Arrays.sort(intervals, (Interval a, interval b) -> {
-        return a.start - b.start;
-    });
+Input: intervals = [[7,10],[2,4]]
+Output: true
  
-    for(int index = 0; index < intervals.length - 1; index++){
-        if(intervals[index].end > intervals[index + 1].start){
-            return false;
+
+Constraints:
+
+0 <= intervals.length <= 10^4
+intervals[i].length == 2
+0 <= starti < endi <= 10^6
+*/
+
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        
+        boolean discrete = true;
+        
+        for (int index = 0; index < intervals.length - 1; ++index) {
+            if (intervals[index][1] > intervals[index + 1][0]) {
+                discrete = false;
+                break;
+            }      
         }
+      
+        return discrete;
     }
- 
-    return true;
 }
