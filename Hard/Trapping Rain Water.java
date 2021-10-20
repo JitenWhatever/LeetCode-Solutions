cbdf312ab7c93e144dd3b89d45f4bdcb94b8bf62
JitenWhatever <jitendra1551k@gmail.com>
2021-10-20 22:05:19 +0530

    two pointer

--- Hard/Trapping Rain Water.java
+++ Hard/Trapping Rain Water.java
@@ -62,20 +62,27 @@ int trap(vector<int>& height)
     return ans;
 }
 
-int trap(vector<int>& height)
-{
-    int left = 0, right = height.size() - 1;
-    int ans = 0;
-    int left_max = 0, right_max = 0;
-    while (left < right) {
-        if (height[left] < height[right]) {
-            height[left] >= left_max ? (left_max = height[left]) : ans += (left_max - height[left]);
-            ++left;
+class Solution {
+    public int trap(int[] height) {
+        int left = 0, right = height.length - 1, left_max = 0, right_max = 0, result = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= left_max) {
+                    left_max = height[left];
+                } else {
+                    result += (left_max - height[left]);
+                }
+                ++left;
+            } else {
+                if (height[right] >= right_max) {
+                    right_max = height[right];
+                } else {
+                    result += (right_max - height[right]);
+                } 
+                --right;
+            }
         }
-        else {
-            height[right] >= right_max ? (right_max = height[right]) : ans += (right_max - height[right]);
-            --right;
-        }
+        
+        return result;
     }
-    return ans;
 }
