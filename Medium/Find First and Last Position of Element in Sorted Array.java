b986b45a40282678e846075147e2eaed69c5d75a
JitenWhatever <jitendra1551k@gmail.com>
2021-11-04 21:22:30 +0530

    binarySearch

--- Medium/Find First and Last Position of Element in Sorted Array.java
+++ Medium/Find First and Last Position of Element in Sorted Array.java
@@ -28,4 +28,49 @@ Constraints:
 nums is a non-decreasing array.
 -10^9 <= target <= 10^9
 */
+  
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int firstIndex = this.binarySearch(nums, target, true);
+        
+        if (firstIndex == -1) {
+            return new int[]{-1, -1};
+        }
+        
+        int lastIndex = this.binarySearch(nums, target, false);
+        
+        return new int[]{firstIndex, lastIndex};
+    }
+    
+    private int binarySearch(int[] nums, int target, boolean isFirst) {
+        int low = 0, high = nums.length - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                if (isFirst) {
 
+                    if (mid == low || nums[mid - 1] != target) {
+                        return mid;
+                    }
+
+                    high = mid - 1;
+                }
+                 else {
+                    if (mid == high || nums[mid + 1] != target) {
+                        return mid;
+                    }
+
+                    low = mid + 1;
+                }
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return -1;
+           
+    }
+}
