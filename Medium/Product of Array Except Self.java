2c1641eeb520ca873b5359c2ac7be55eb98579fb
JitenWhatever <jitendra1551k@gmail.com>
2021-11-07 21:25:33 +0530

    simple

--- Medium/Product of Array Except Self.java
+++ Medium/Product of Array Except Self.java
@@ -1,17 +1,32 @@
 /*
-Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+Given an integer array nums, 
+return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
 
-Example:
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 
-Input:  [1,2,3,4]
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
 Output: [24,12,8,6]
+Example 2:
 
-Constraint: It's guaranteed that the product of the elements of any prefix or suffix of the array (including the whole array) fits in a 32 bit integer.
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+ 
 
-Note: Please solve it without division and in O(n).
+Constraints:
 
-Follow up:
-Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
+2 <= nums.length <= 10^5
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ 
+
+Follow up: Can you solve the problem in O(1) extra space complexity? 
+(The output array does not count as extra space for space complexity analysis.)
 */
 
 class Solution {
