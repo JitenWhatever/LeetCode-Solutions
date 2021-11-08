32257087803bf052a67b95318e0afc81df328c0c
JitenWhatever <jitendra1551k@gmail.com>
2021-11-08 22:16:49 +0530

    dfs

--- Medium/Word Search.java
+++ Medium/Word Search.java
@@ -1,60 +1,81 @@
 /*
-Given a 2D board and a word, find if the word exists in the grid.
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
 
-The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. 
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. 
 The same letter cell may not be used more than once.
 
-Example:
+Example 1:
+Input: board = [
+    ["A","B","C","E"],
+    ["S","F","C","S"],
+    ["A","D","E","E"]
+    ], 
+    word = "ABCCED"
+Output: true
 
-board =
-[
-  ['A','B','C','E'],
-  ['S','F','C','S'],
-  ['A','D','E','E']
-]
+Example 2:
 
-Given word = "ABCCED", return true.
-Given word = "SEE", return true.
-Given word = "ABCB", return false.
+Input: board = [
+    ["A","B","C","E"],
+    ["S","F","C","S"],
+    ["A","D","E","E"]
+    ], 
+    word = "SEE"
+Output: true
+
+Example 3:
+Input: board = [
+    ["A","B","C","E"],
+    ["S","F","C","S"],
+    ["A","D","E","E"]
+    ], word = "ABCB"
+Output: false
  
 
 Constraints:
 
-board and word consists only of lowercase and uppercase English letters.
-1 <= board.length <= 200
-1 <= board[i].length <= 200
-1 <= word.length <= 10^3
+m == board.length
+n = board[i].length
+1 <= m, n <= 6
+1 <= word.length <= 15
+board and word consists of only lowercase and uppercase English letters.
+ 
+
+Follow up: Could you use search pruning to make your solution faster with a larger board?
 */
 
 class Solution {
-    
-    private boolean dfs(int r, int c, char[][] board, int currentIndex, String word) {
-        
-        if(currentIndex == word.length()) {
-            return true;
-        }
-        
-        if(r < 0 || r >= board.length || c < 0 || c >= board[r].length || board[r][c] != word.charAt(currentIndex)) {
-            return false;
-        }
-        
-        char ch = board[r][c];
-        board[r][c] = '#';
-
-        ++currentIndex;
-        boolean found = ( dfs(r + 1, c, board, currentIndex, word) || dfs(r - 1, c, board, currentIndex, word) || dfs(r, c + 1, board, currentIndex, word) || dfs(r, c - 1, board, currentIndex, word) );
-        board[r][c] = ch;
-        return found;   
-    }
     public boolean exist(char[][] board, String word) {
-        
-        for(int r = 0; r < board.length; ++r) {
-            for(int c = 0; c < board[r].length; ++c) {
-                if(board[r][c] == word.charAt(0) && dfs(r, c, board, 0, word)) {
+    
+        for (int r = 0; r < board.length; ++r) {
+            for (int c = 0; c < board[r].length; ++c) {
+                if (word.charAt(0) == board[r][c] && dfs(board, r, c, 0, word)) {
                     return true;
                 }
             }
         }
+        
         return false;
+    }
+    
+    private boolean dfs(char[][] board, int r, int c, int currentIndex, String word) {
+        
+        if (currentIndex >= word.length()) {
+            return true;
+        }
+        
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] == '*' || word.charAt(currentIndex) != board[r][c]) {
+            return false;
+        }
+        
+        char ch = board[r][c];
+        board[r][c] = '*';
+        
+        boolean found = (dfs(board, r + 1, c, currentIndex + 1, word) || dfs(board, r - 1, c, currentIndex + 1, word) || 
+                        dfs(board, r, c + 1, currentIndex + 1, word) || dfs(board, r, c - 1, currentIndex + 1, word));
+        
+        board[r][c] = ch;
+        
+        return found;
     }
 }
