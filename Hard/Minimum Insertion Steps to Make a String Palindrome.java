d991841c8290907102d9daf094f267c710ee8a63
JitenWhatever <jitendra1551k@gmail.com>
2022-01-22 00:11:29 +0530

    dp

--- Hard/Minimum Insertion Steps to Make a String Palindrome.java
+++ Hard/Minimum Insertion Steps to Make a String Palindrome.java
@@ -73,3 +73,25 @@ class Solution {
         return dp[low][high];
     }
 }
+
+
+class Solution {
+    public int minInsertions(String s) {
+        int len = s.length();
+         int[][] dp = new int[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = 0;
+        }
+        char[] sa = s.toCharArray();
+        for (int i = len - 2; i >=0 ; i--) {
+            for (int j = i + 1; j < len; j++) {
+                if (sa[i] == sa[j]){
+                    dp[i][j] = dp[i+1][j-1];
+                }else {
+                    dp[i][j] = Math.min(dp[i+1][j],dp[i][j-1]) +1;
+                }
+            }
+        }
+        return dp[0][len-1];
+    }
+}
