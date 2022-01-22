48e76d0e7c2054d5835048509256178bff7527a0
JitenWhatever <jitendra1551k@gmail.com>
2022-01-23 02:44:58 +0530

    remove ) then (

---
+++ Medium/Minimum Remove to Make Valid Parentheses.java
@@ -0,0 +1,126 @@
+/*
+Given a string s of '(' , ')' and lowercase English characters.
+
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) 
+so that the resulting parentheses string is valid and return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+ 
+
+Example 1:
+
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+Example 2:
+
+Input: s = "a)b(c)d"
+Output: "ab(c)d"
+Example 3:
+
+Input: s = "))(("
+Output: ""
+Explanation: An empty string is also valid.
+ 
+
+Constraints:
+
+1 <= s.length <= 10^5
+s[i] is either'(' , ')', or lowercase English letter.
+*/
+
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder();
+        
+        Set<Integer> indexToRemove = new HashSet<>();
+        Stack<Integer> stck = new Stack<>();
+        
+        for (int index = 0; index < s.length(); ++index) {
+            if (s.charAt(index) == '(') {
+                stck.push(index);
+            } else if (s.charAt(index) == ')') {
+                if (stck.isEmpty()) {
+                    indexToRemove.add(index);
+                } else {
+                    stck.pop();
+                }
+            }
+        }
+        
+        while(!stck.isEmpty()) indexToRemove.add(stck.pop());
+        
+        for (int index = 0; index < s.length(); ++index) {
+            if (!indexToRemove.contains(index)) {
+                sb.append(s.charAt(index));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
+
+class Solution {
+
+    private StringBuilder removeInvalidClosing(CharSequence string, char open, char close) {
+        StringBuilder sb = new StringBuilder();
+        int balance = 0;
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            if (c == open) {
+                balance++;
+            } if (c == close) {
+                if (balance == 0) continue;
+                balance--;
+            }
+            sb.append(c);
+        }  
+        return sb;
+    }
+
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder result = removeInvalidClosing(s, '(', ')');
+        result = removeInvalidClosing(result.reverse(), ')', '(');
+        return result.reverse().toString();
+    }
+}
+
+class Solution {
+
+    public String minRemoveToMakeValid(String s) {
+
+        // Pass 1: Remove all invalid ")"
+        StringBuilder sb = new StringBuilder();
+        int openSeen = 0;
+        int balance = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                openSeen++;
+                balance++;
+            } if (c == ')') {
+                if (balance == 0) continue;
+                balance--;
+            }
+            sb.append(c);
+        }
+
+        // Pass 2: Remove the rightmost "("
+        StringBuilder result = new StringBuilder();
+        int openToKeep = openSeen - balance;
+        for (int i = 0; i < sb.length(); i++) {
+            char c = sb.charAt(i);
+            if (c == '(') {
+                openToKeep--;
+                if (openToKeep < 0) continue;
+            }
+            result.append(c);
+        }
+
+        return result.toString();
+    }
+}
