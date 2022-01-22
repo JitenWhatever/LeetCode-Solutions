9210d6746dc62fecfd8b5d21269567aa3132f542
JitenWhatever <jitendra1551k@gmail.com>
2022-01-23 02:51:07 +0530

    simple

--- Easy/Valid Parentheses.java
+++ Easy/Valid Parentheses.java
@@ -1,33 +1,30 @@
 /*
-Given a string containing just the characters '(', ')', '{', '}', '[' and ']', 
-determine if the input string is valid.
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
 An input string is valid if:
 
 Open brackets must be closed by the same type of brackets.
 Open brackets must be closed in the correct order.
-Note that an empty string is also considered valid.
+ 
 
 Example 1:
 
-Input: "()"
+Input: s = "()"
 Output: true
 Example 2:
 
-Input: "()[]{}"
+Input: s = "()[]{}"
 Output: true
 Example 3:
 
-Input: "(]"
+Input: s = "(]"
 Output: false
-Example 4:
+ 
 
-Input: "([)]"
-Output: false
-Example 5:
+Constraints:
 
-Input: "{[]}"
-Output: true
+1 <= s.length <= 10^4
+s consists of parentheses only '()[]{}'.
 */
 
 class Solution {
