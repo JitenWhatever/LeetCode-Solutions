bd68bb6692643f39174083ac9ae75aa38779240c
JitenWhatever <jitendra1551k@gmail.com>
2022-01-23 02:53:40 +0530

    sliding window

--- Medium/Longest Substring Without Repeating Characters.java
+++ Medium/Longest Substring Without Repeating Characters.java
@@ -1,23 +1,27 @@
 /*
-Given a string, find the length of the longest substring without repeating characters.
+Given a string s, find the length of the longest substring without repeating characters.
 
 Example 1:
 
-Input: "abcabcbb"
-Output: 3 
-Explanation: The answer is "abc", with the length of 3. 
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
 Example 2:
 
-Input: "bbbbb"
+Input: s = "bbbbb"
 Output: 1
 Explanation: The answer is "b", with the length of 1.
 Example 3:
 
-Input: "pwwkew"
+Input: s = "pwwkew"
 Output: 3
-Explanation: The answer is "wke", with the length of 3. 
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+Constraints:
 
-Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+0 <= s.length <= 5 * 10^4
+s consists of English letters, digits, symbols and spaces.
 */
 
 // as long as we find unique element expand the right boundry of window, and increase left when duplicate element occurs
