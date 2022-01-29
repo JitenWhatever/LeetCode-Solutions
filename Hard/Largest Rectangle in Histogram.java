f75005d4037f622b0c34b0442b8173ec977c3370
JitenWhatever <jitendra1551k@gmail.com>
2022-01-30 04:24:18 +0530

    bf + d&c + two pointer stack

--- Hard/Largest Rectangle in Histogram.java
+++ Hard/Largest Rectangle in Histogram.java
@@ -1,64 +1,128 @@
 /*
-Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, 
+return the area of the largest rectangle in the histogram.
 
- 
-
-https://assets.leetcode.com/uploads/2018/10/12/histogram.png
-Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
-
- 
-
-https://assets.leetcode.com/uploads/2018/10/12/histogram_area.png
-The largest rectangle is shown in the shaded area, which has area = 10 unit.
-
- 
-
-Example:
-
-Input: [2,1,5,6,2,3]
+Example 1:
+Input: heights = [2,1,5,6,2,3]
 Output: 10
+Explanation: The above is a histogram where width of each bar is 1.
+The largest rectangle is shown in the red area, which has an area = 10 unitsAbove is a histogram where width of each bar is 1, 
+given height = [2,1,5,6,2,3].
+
+https://assets.leetcode.com/uploads/2021/01/04/histogram.jpg
+
+Example 2:
+https://assets.leetcode.com/uploads/2021/01/04/histogram-1.jpg
+
+Input: heights = [2,4]
+Output: 4
+ 
+
+Constraints:
+
+1 <= heights.length <= 10^5
+0 <= heights[i] <= 10^4
 */
 
-class Solution {
+public class Solution {
     public int largestRectangleArea(int[] heights) {
-     
-        if(heights == null || heights.length == 0) {
-            return 0;
-        }
-        
-        int largestRectangleArea = Integer.MIN_VALUE;
-        
-        Stack<Integer> st = new Stack();
-        
-        int index = 0;
-        while(index < heights.length) {
-            if(st.empty() || heights[st.peek()] <= heights[index]) {
-                st.push(index++);
-            }
-            else {
-                int top = st.pop();
-                
-                if(st.empty()) {
-                    largestRectangleArea = Math.max(largestRectangleArea, index * heights[top]); 
-                }
-                else {
-                    largestRectangleArea = Math.max(largestRectangleArea, (index - st.peek() - 1) * heights[top]);
-                }
+        int maxarea = 0;
+        for (int i = 0; i < heights.length; i++) {
+            for (int j = i; j < heights.length; j++) {
+                int minheight = Integer.MAX_VALUE;
+                for (int k = i; k <= j; k++)
+                    minheight = Math.min(minheight, heights[k]);
+                maxarea = Math.max(maxarea, minheight * (j - i + 1));
             }
         }
-        
-        while(!st.empty()) {
-            int top = st.pop();
-                
-            if(st.empty()) {
-                largestRectangleArea = Math.max(largestRectangleArea, index * heights[top]); 
-            }
-            else {
-                largestRectangleArea = Math.max(largestRectangleArea, (index - st.peek() - 1) * heights[top]);
+        return maxarea;
+    }
+}
+ 
+
+public class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int maxArea = 0;
+        int length = heights.length;
+        for (int i = 0; i < length; i++) {
+            int minHeight = Integer.MAX_VALUE;
+            for (int j = i; j < length; j++) {
+                minHeight = Math.min(minHeight, heights[j]);
+                maxArea = Math.max(maxArea, minHeight * (j - i + 1));
             }
         }
-        
-        return largestRectangleArea;
+        return maxArea;
     }
 }
 
+public class Solution {
+    public int calculateArea(int[] heights, int start, int end) {
+        if (start > end)
+            return 0;
+        int minindex = start;
+        for (int i = start; i <= end; i++)
+            if (heights[minindex] > heights[i])
+                minindex = i;
+        return Math.max(heights[minindex] * (end - start + 1),
+                        Math.max(calculateArea(heights, start, minindex - 1),
+                                calculateArea(heights, minindex + 1, end))
+                );
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        return calculateArea(heights, 0, heights.length - 1);
+    }
+}
+
+public class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(-1);
+        int length = heights.length;
+        int maxArea = 0;
+        for (int i = 0; i < length; i++) {
+            while ((stack.peek() != -1)
+                    && (heights[stack.peek()] >= heights[i])) {
+                int currentHeight = heights[stack.pop()];
+                int currentWidth = i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, currentHeight * currentWidth);
+            }
+            stack.push(i);
+        }
+        while (stack.peek() != -1) {
+            int currentHeight = heights[stack.pop()];
+            int currentWidth = length - stack.peek() - 1;
+            maxArea = Math.max(maxArea, currentHeight * currentWidth);
+        }
+        return maxArea;
+    }
+}
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        if (heights == null || heights.length == 0) {
+            return 0;
+        }
+        
+        int index = 0, len = heights.length, maxArea = Integer.MIN_VALUE;
+        Stack<Integer> stck = new Stack<>();
+        while (index < len) {
+            if (stck.isEmpty() || heights[index] >= heights[stck.peek()]) {
+                stck.add(index++);
+            } else {
+                int top = stck.pop();
+                
+                int area = heights[top] * (stck.isEmpty() ? index : index - stck.peek() - 1);
+                maxArea = Math.max(area, maxArea);
+            }
+        }
+        while (!stck.isEmpty()) {
+           int top = stck.pop();
+
+            int area = heights[top] * (stck.isEmpty() ? index : index - stck.peek() - 1);
+            maxArea = Math.max(area, maxArea);
+        }
+        
+        return maxArea;
+    }
+}
