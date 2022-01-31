fb537683e58edce20c8ccadd9b865fa6c71365c0
JitenWhatever <jitendra1551k@gmail.com>
2022-01-31 22:49:26 +0530

    find l < r then swap with least greater then reverse rest

--- Medium/NextPermutation.java
+++ Medium/NextPermutation.java
@@ -1,11 +1,21 @@
-/*
-Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+import java.util.Arrays;
 
-If such an arrangement is not possible, it must rearrange it as the lowest possible order (i.e., sorted in ascending order).
+/*
+A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+For example, for arr = [1,2,3], the following are considered permutations of arr: [1,2,3], [1,3,2], [3,1,2], [2,3,1].
+The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally, 
+if all the permutations of the array are sorted in one container according to their lexicographical order, 
+then the next permutation of that array is the permutation that follows it in the sorted container. 
+If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+For example, the next permutation of arr = [1,2,3] is [1,3,2].
+Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+Given an array of integers nums, find the next permutation of nums.
 
 The replacement must be in place and use only constant extra memory.
 
- 
 Example 1:
 
 Input: nums = [1,2,3]
@@ -18,20 +28,15 @@ Example 3:
 
 Input: nums = [1,1,5]
 Output: [1,5,1]
-Example 4:
-
-Input: nums = [1]
-Output: [1]
  
-
 Constraints:
 
 1 <= nums.length <= 100
 0 <= nums[i] <= 100
 */
 
-class Solution {
-    public void nextPermutation(int[] nums) {
+class NextPermutation {
+    public void nextPermutation2(int[] nums) {
         int index = nums.length - 1;
         
         while (index > 0 && nums[index] <= nums[index - 1]) {
@@ -59,15 +64,7 @@ class Solution {
         
     }
     
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-}
 
-
-public class Solution {
     public void nextPermutation(int[] nums) {
         int i = nums.length - 2;
         while (i >= 0 && nums[i + 1] <= nums[i]) {
