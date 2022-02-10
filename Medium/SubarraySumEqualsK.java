349eb2d56ae0ef6acb18b157aa18a5710bc87f86
JitenWhatever <jitendra1551k@gmail.com>
2022-02-10 22:54:10 +0530

    hashmap

---
+++ Medium/SubarraySumEqualsK.java
@@ -0,0 +1,83 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k.
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+ 
+Constraints:
+
+1 <= nums.length <= 2 * 10^4
+-1000 <= nums[i] <= 1000
+-107 <= k <= 10^7
+*/
+
+
+class SubarraySumEqualsK {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> hash = new HashMap<Integer, Integer>();
+        Integer sum = 0, subArrays = 0;;    
+        hash.put(0, 1);
+        
+        for(Integer num : nums) {
+            sum += num;
+            if(hash.getOrDefault(sum - k, 0) != 0){
+                subArrays += hash.get(sum - k);
+            }
+            
+            hash.put(sum, hash.getOrDefault(sum, 0) + 1);
+        }
+        
+        return subArrays;
+    }
+
+    public int subarraySum1(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; start++) {
+            for (int end = start + 1; end <= nums.length; end++) {
+                int sum = 0;
+                for (int i = start; i < end; i++)
+                    sum += nums[i];
+                if (sum == k)
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    public int subarraySum2(int[] nums, int k) {
+        int count = 0;
+        int[] sum = new int[nums.length + 1];
+        sum[0] = 0;
+        for (int i = 1; i <= nums.length; i++)
+            sum[i] = sum[i - 1] + nums[i - 1];
+        for (int start = 0; start < nums.length; start++) {
+            for (int end = start + 1; end <= nums.length; end++) {
+                if (sum[end] - sum[start] == k)
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    public int subarraySum3(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; start++) {
+            int sum=0;
+            for (int end = start; end < nums.length; end++) {
+                sum+=nums[end];
+                if (sum == k)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
