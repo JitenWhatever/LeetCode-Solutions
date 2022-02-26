e6c0c9b2411b0c3a1de8a6264a631114dfc5cec1
JitenWhatever <jitendra1551k@gmail.com>
2022-02-26 20:52:58 +0530

    dfs + bfs + bitmask

---
+++ Hard/ShortestPathVisitingAllNodes.java
@@ -0,0 +1,118 @@
+import java.util.ArrayList;
+
+/*
+You have an undirected, connected graph of n nodes labeled from 0 to n - 1.
+You are given an array graph where graph[i] is a list of all the nodes connected with node i by an edge.
+
+Return the length of the shortest path that visits every node. 
+You may start and stop at any node, you may revisit nodes multiple times, and you may reuse edges.
+
+Example 1:
+https://assets.leetcode.com/uploads/2021/05/12/shortest1-graph.jpg
+
+Input: graph = [[1,2,3],[0],[0],[0]]
+Output: 4
+Explanation: One possible path is [1,0,2,0,3]
+
+Example 2:
+https://assets.leetcode.com/uploads/2021/05/12/shortest2-graph.jpg
+
+Input: graph = [[1],[0,2,4],[1,3,4],[2],[1,2]]
+Output: 4
+Explanation: One possible path is [0,1,4,2,3]
+ 
+
+Constraints:
+
+n == graph.length
+1 <= n <= 12
+0 <= graph[i].length < n
+graph[i] does not contain i.
+If graph[a] contains b, then graph[b] contains a.
+The input graph is always connected.
+*/
+
+class ShortestPathVisitingAllNodes {
+    private int[][] cache;
+    private int endingMask;
+    
+    public int dp(int node, int mask, int[][] graph) {
+        if (cache[node][mask] != 0) {
+            return cache[node][mask];
+        }
+        if ((mask & (mask - 1)) == 0) {
+            // Base case - mask only has a single "1", which means
+            // that only one node has been visited (the current node)
+            return 0;
+        }
+        
+        cache[node][mask] = Integer.MAX_VALUE - 1; // Avoid infinite loop in recursion
+        for (int neighbor: graph[node]) {
+            if ((mask & (1 << neighbor)) != 0) {
+                int alreadyVisited = dp(neighbor, mask, graph);
+                int notVisited = dp(neighbor, mask ^ (1 << node), graph);
+                int betterOption = Math.min(alreadyVisited, notVisited);
+                cache[node][mask] = Math.min(cache[node][mask], 1 + betterOption);
+            }
+        }
+        
+        return cache[node][mask];
+    }
+    
+    public int shortestPathLength(int[][] graph) {
+        int n = graph.length;
+        endingMask = (1 << n) - 1;
+        cache = new int[n + 1][endingMask + 1];
+        
+        int best = Integer.MAX_VALUE;
+        for (int node = 0; node < n; node++) {
+            best = Math.min(best, dp(node, endingMask, graph));
+        }
+        
+        return best;
+    }
+
+    public int shortestPathLength1(int[][] graph) {
+        if (graph.length == 1) {
+            return 0;
+        }
+        
+        int n = graph.length;
+        int endingMask = (1 << n) - 1;
+        boolean[][] seen = new boolean[n][endingMask];
+        ArrayList<int[]> queue = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            queue.add(new int[] {i, 1 << i});
+            seen[i][1 << i] = true;
+        }
+        
+        int steps = 0;
+        while (!queue.isEmpty()) {
+            ArrayList<int[]> nextQueue = new ArrayList<>();
+            for (int i = 0; i < queue.size(); i++) {
+                int[] currentPair = queue.get(i);
+                int node = currentPair[0];
+                int mask = currentPair[1];
+                for (int neighbor : graph[node]) {
+                    int nextMask = mask | (1 << neighbor);
+                    if (nextMask == endingMask) {
+                        return 1 + steps;
+                    }
+                    
+                    if (!seen[neighbor][nextMask]) {
+                        seen[neighbor][nextMask] = true;
+                        nextQueue.add(new int[] {neighbor, nextMask});
+                    }
+                }
+            }
+            steps++;
+            queue = nextQueue;
+        }
+        
+        return -1;
+    }
+}
+
+// Space complexity: O(2^N⋅N)
+// Time complexit: O(2^N⋅N^2)
