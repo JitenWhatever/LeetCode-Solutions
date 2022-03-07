e12dbf0071010c560bbf79c046959e9c89002d3d
JitenWhatever <jitendra1551k@gmail.com>
2022-03-07 22:21:06 +0530

    simple

--- Easy/MergeTwoSortedLists.java
+++ Easy/MergeTwoSortedLists.java
@@ -1,19 +1,24 @@
+import java.util.Objects;
+
 /*
-Merge two sorted linked lists and return it as a sorted list. 
-The list should be made by splicing together the nodes of the first two lists.
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
 
 Example 1:
 https://assets.leetcode.com/uploads/2020/10/03/merge_ex1.jpg
 
-Input: l1 = [1,2,4], l2 = [1,3,4]
+Input: list1 = [1,2,4], list2 = [1,3,4]
 Output: [1,1,2,3,4,4]
 Example 2:
 
-Input: l1 = [], l2 = []
+Input: list1 = [], list2 = []
 Output: []
 Example 3:
 
-Input: l1 = [], l2 = [0]
+Input: list1 = [], list2 = [0]
 Output: [0]
  
 
@@ -21,21 +26,21 @@ Constraints:
 
 The number of nodes in both lists is in the range [0, 50].
 -100 <= Node.val <= 100
-Both l1 and l2 are sorted in non-decreasing order.
+Both list1 and list2 are sorted in non-decreasing order.
 */
 
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
+
+
+class MergeTwoSortedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode();
         ListNode currentNode = dummy;
@@ -56,10 +61,8 @@ class Solution {
         
         return dummy.next;
     }
-}
 
-class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
         if (l1 == null) {
             return l2;
         }
