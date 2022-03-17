021e7df8eb010f49669181c5275ca5b88291dc26
JitenWhatever <jitendra1551k@gmail.com>
2022-03-17 22:13:02 +0530

    d&c, stack, score count

---
+++ Medium/ScoreOfParentheses.java
@@ -0,0 +1,94 @@
+import java.util.Stack;
+
+/*
+Given a balanced parentheses string s, return the score of the string.
+
+The score of a balanced parentheses string is based on the following rule:
+
+"()" has score 1.
+AB has score A + B, where A and B are balanced parentheses strings.
+(A) has score 2 * A, where A is a balanced parentheses string.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: 1
+Example 2:
+
+Input: s = "(())"
+Output: 2
+Example 3:
+
+Input: s = "()()"
+Output: 2
+ 
+
+Constraints:
+
+2 <= s.length <= 50
+s consists of only '(' and ')'.
+s is a balanced parentheses string.
+*/
+
+public class ScoreOfParentheses {
+   
+
+    public int scoreOfParentheses(String S) {
+        return F(S, 0, S.length());
+    }
+
+    public int F(String S, int i, int j) {
+        //Score of balanced string S[i:j]
+        int ans = 0, bal = 0;
+
+        // Split string into primitives
+        for (int k = i; k < j; ++k) {
+            bal += S.charAt(k) == '(' ? 1 : -1;
+            if (bal == 0) {
+                if (k - i == 1) ans++;
+                else ans += 2 * F(S, i+1, k);
+                i = k+1;
+            }
+        }
+
+        return ans;
+    }
+
+    // O(N^2)
+
+    public int scoreOfParentheses1(String S) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0); // The score of the current frame
+    
+        for (char c: S.toCharArray()) {
+            if (c == '(')
+                stack.push(0);
+            else {
+                int v = stack.pop();
+                int w = stack.pop();
+                stack.push(w + Math.max(2 * v, 1));
+            }
+        }
+    
+        return stack.pop();
+    }
+
+
+
+    public int scoreOfParentheses2(String S) {
+        int ans = 0, bal = 0;
+        for (int i = 0; i < S.length(); ++i) {
+            if (S.charAt(i) == '(') {
+                bal++;
+            } else {
+                bal--;
+                if (S.charAt(i-1) == '(')
+                    ans += 1 << bal;
+            }
+        }
+
+        return ans;
+    }
+    
+}
