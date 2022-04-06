39d4f532c935ba41d1a166bbf61826cad4288bfe
JitenWhatever <jitendra1551k@gmail.com>
2022-04-06 20:53:07 +0530

    2 pointer

---
+++ Medium/3SumWithMultiplicity.java
@@ -0,0 +1,190 @@
+import java.util.Arrays;
+
+/*
+Given an integer array arr, and an integer target, 
+return the number of tuples i, j, k such that i < j < k and arr[i] + arr[j] + arr[k] == target.
+
+As the answer can be very large, return it modulo 10^9 + 7.
+
+Example 1:
+
+Input: arr = [1,1,2,2,3,3,4,4,5,5], target = 8
+Output: 20
+Explanation: 
+Enumerating by the values (arr[i], arr[j], arr[k]):
+(1, 2, 5) occurs 8 times;
+(1, 3, 4) occurs 8 times;
+(2, 2, 4) occurs 2 times;
+(2, 3, 3) occurs 2 times.
+Example 2:
+
+Input: arr = [1,1,2,2,2,2], target = 5
+Output: 12
+Explanation: 
+arr[i] = 1, arr[j] = arr[k] = 2 occurs 12 times:
+We choose one 1 from [1,1] in 2 ways,
+and two 2s from [2,2,2,2] in 6 ways.
+ 
+
+Constraints:
+
+3 <= arr.length <= 3000
+0 <= arr[i] <= 100
+0 <= target <= 300
+*/
+
+class Sum3WithMultiplicity {
+
+    public int threeSumMulti(int[] A, int target) {
+        int MOD = 1_000_000_007;
+        long ans = 0;
+        Arrays.sort(A);
+
+        for (int i = 0; i < A.length; ++i) {
+            // We'll try to find the number of i < j < k
+            // with A[j] + A[k] == T, where T = target - A[i].
+
+            // The below is a "two sum with multiplicity".
+            int T = target - A[i];
+            int j = i+1, k = A.length - 1;
+
+            while (j < k) {
+                // These steps proceed as in a typical two-sum.
+                if (A[j] + A[k] < T)
+                    j++;
+                else if (A[j] + A[k] > T)
+                    k--;
+                else if (A[j] != A[k]) {  // We have A[j] + A[k] == T.
+                    // Let's count "left": the number of A[j] == A[j+1] == A[j+2] == ...
+                    // And similarly for "right".
+                    int left = 1, right = 1;
+                    while (j+1 < k && A[j] == A[j+1]) {
+                        left++;
+                        j++;
+                    }
+                    while (k-1 > j && A[k] == A[k-1]) {
+                        right++;
+                        k--;
+                    }
+
+                    ans += left * right;
+                    ans %= MOD;
+                    j++;
+                    k--;
+                } else {
+                    // M = k - j + 1
+                    // We contributed M * (M-1) / 2 pairs.
+                    ans += (k-j+1) * (k-j) / 2;
+                    ans %= MOD;
+                    break;
+                }
+            }
+        }
+
+        return (int) ans;
+    }
+
+    public int threeSumMulti1(int[] A, int target) {
+        int MOD = 1_000_000_007;
+        long[] count = new long[101];
+        for (int x: A)
+            count[x]++;
+
+        long ans = 0;
+
+        // All different
+        for (int x = 0; x <= 100; ++x)
+            for (int y = x+1; y <= 100; ++y) {
+                int z = target - x - y;
+                if (y < z && z <= 100) {
+                    ans += count[x] * count[y] * count[z];
+                    ans %= MOD;
+                }
+            }
+
+        // x == y != z
+        for (int x = 0; x <= 100; ++x) {
+            int z = target - 2*x;
+            if (x < z && z <= 100) {
+                ans += count[x] * (count[x] - 1) / 2 * count[z];
+                ans %= MOD;
+            }
+        }
+
+        // x != y == z
+        for (int x = 0; x <= 100; ++x) {
+            if (target % 2 == x % 2) {
+                int y = (target - x) / 2;
+                if (x < y && y <= 100) {
+                    ans += count[x] * count[y] * (count[y] - 1) / 2;
+                    ans %= MOD;
+                }
+            }
+        }
+
+        // x == y == z
+        if (target % 3 == 0) {
+            int x = target / 3;
+            if (0 <= x && x <= 100) {
+                ans += count[x] * (count[x] - 1) * (count[x] - 2) / 6;
+                ans %= MOD;
+            }
+        }
+
+        return (int) ans;
+    }
+
+    public int threeSumMulti2(int[] A, int target) {
+        int MOD = 1_000_000_007;
+
+        // Initializing as long saves us the trouble of
+        // managing count[x] * count[y] * count[z] overflowing later.
+        long[] count = new long[101];
+        int uniq = 0;
+        for (int x: A) {
+            count[x]++;
+            if (count[x] == 1)
+                uniq++;
+        }
+
+        int[] keys = new int[uniq];
+        int t = 0;
+        for (int i = 0; i <= 100; ++i)
+            if (count[i] > 0)
+                keys[t++] = i;
+
+        long ans = 0;
+        // Now, let's do a 3sum on "keys", for i <= j <= k.
+        // We will use count to add the correct contribution to ans.
+
+        for (int i = 0; i < keys.length; ++i) {
+            int x = keys[i];
+            int T = target - x;
+            int j = i, k = keys.length - 1;
+            while (j <= k) {
+                int y = keys[j], z = keys[k];
+                if (y + z < T) {
+                    j++;
+                } else if (y + z > T) {
+                    k--;
+                } else {  // # x+y+z == T, now calc the size of the contribution
+                    if (i < j && j < k) {
+                        ans += count[x] * count[y] * count[z];
+                    } else if (i == j && j < k) {
+                        ans += count[x] * (count[x] - 1) / 2 * count[z];
+                    } else if (i < j && j == k) {
+                        ans += count[x] * count[y] * (count[y] - 1) / 2;
+                    } else {  // i == j == k
+                        ans += count[x] * (count[x] - 1) * (count[x] - 2) / 6;
+                    }
+
+                    ans %= MOD;
+                    j++;
+                    k--;
+                }
+            }
+        }
+
+        return (int) ans;
+    }
+}
