0928ab838fa9928986f5ad3b5570f660450a169e
Jitendra Kumar <jitendra1551k@gmail.com>
2022-05-03 21:57:10 +0530

    stack

---
+++ Medium/ShortestUnsortedContinuousSubarray.java
@@ -0,0 +1,141 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+/*
+Given an integer array nums, you need to find one continuous subarray that 
+if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order.
+
+Return the shortest such subarray and output its length.
+
+Example 1:
+
+Input: nums = [2,6,4,8,10,9,15]
+Output: 5
+Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: 0
+
+Example 3:
+
+Input: nums = [1]
+Output: 0
+
+Constraints:
+
+1 <= nums.length <= 10^4
+-10^5 <= nums[i] <= 10^5
+
+Follow up: Can you solve it in O(n) time complexity?
+*/
+
+public class ShortestUnsortedContinuousSubarray {
+    
+    public int findUnsortedSubarray(int[] nums) {
+        int res = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j <= nums.length; j++) {
+                int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE, prev = Integer.MIN_VALUE;
+                for (int k = i; k < j; k++) {
+                    min = Math.min(min, nums[k]);
+                    max = Math.max(max, nums[k]);
+                }
+                if ((i > 0 && nums[i - 1] > min) || (j < nums.length && nums[j] < max))
+                    continue;
+                int k = 0;
+                while (k < i && prev <= nums[k]) {
+                    prev = nums[k];
+                    k++;
+                }
+                if (k != i)
+                    continue;
+                k = j;
+                while (k < nums.length && prev <= nums[k]) {
+                    prev = nums[k];
+                    k++;
+                }
+                if (k == nums.length) {
+                    res = Math.min(res, j - i);
+
+                }
+            }
+        }
+        return res;
+    }
+
+    
+    public int findUnsortedSubarray1(int[] nums) {
+        int l = nums.length, r = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[i]) {
+                    r = Math.max(r, j);
+                    l = Math.min(l, i);
+                }
+            }
+        }
+        return r - l < 0 ? 0 : r - l + 1;
+    }
+ 
+   
+    public int findUnsortedSubarray2(int[] nums) {
+        int[] snums = nums.clone();
+        Arrays.sort(snums);
+        int start = snums.length, end = 0;
+        for (int i = 0; i < snums.length; i++) {
+            if (snums[i] != nums[i]) {
+                start = Math.min(start, i);
+                end = Math.max(end, i);
+            }
+        }
+        return (end - start >= 0 ? end - start + 1 : 0);
+    }
+    
+
+    public int findUnsortedSubarray3(int[] nums) {
+        Stack < Integer > stack = new Stack < Integer > ();
+        int l = nums.length, r = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] > nums[i])
+                l = Math.min(l, stack.pop());
+            stack.push(i);
+        }
+        stack.clear();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i])
+                r = Math.max(r, stack.pop());
+            stack.push(i);
+        }
+        return r - l > 0 ? r - l + 1 : 0;
+    }
+
+    public int findUnsortedSubarray4(int[] nums) {
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        boolean flag = false;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1])
+                flag = true;
+            if (flag)
+                min = Math.min(min, nums[i]);
+        }
+        flag = false;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] > nums[i + 1])
+                flag = true;
+            if (flag)
+                max = Math.max(max, nums[i]);
+        }
+        int l, r;
+        for (l = 0; l < nums.length; l++) {
+            if (min < nums[l])
+                break;
+        }
+        for (r = nums.length - 1; r >= 0; r--) {
+            if (max > nums[r])
+                break;
+        }
+        return r - l < 0 ? 0 : r - l + 1;
+    }
+}
