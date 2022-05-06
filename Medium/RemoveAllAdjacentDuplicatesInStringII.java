139551170e05615bbae11413f4dd95002c9549fb
Jitendra Kumar <jitendra1551k@gmail.com>
2022-05-06 23:02:22 +0530

    stack

---
+++ Medium/RemoveAllAdjacentDuplicatesInStringII.java
@@ -0,0 +1,141 @@
+import java.util.Stack;
+
+/*
+You are given a string s and an integer k, 
+a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, 
+causing the left and the right side of the deleted substring to concatenate together.
+
+We repeatedly make k duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
+
+Example 1:
+
+Input: s = "abcd", k = 2
+Output: "abcd"
+Explanation: There's nothing to delete.
+
+Example 2:
+
+Input: s = "deeedbbcccbdaa", k = 3
+Output: "aa"
+Explanation: 
+First delete "eee" and "ccc", get "ddbbbdaa"
+Then delete "bbb", get "dddaa"
+Finally delete "ddd", get "aa"
+
+Example 3:
+
+Input: s = "pbbcggttciiippooaais", k = 2
+Output: "ps"
+
+Constraints:
+    1 <= s.length <= 10^5
+    2 <= k <= 10^4
+    s only contains lower case English letters.
+*/
+
+public class RemoveAllAdjacentDuplicatesInStringII {
+
+    public String removeDuplicates(String s, int k) {
+        StringBuilder sb = new StringBuilder(s);
+        int length = -1;
+        while (length != sb.length()) {
+            length = sb.length();
+            for (int i = 0, count = 1; i < sb.length(); ++i) {
+                if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
+                    count = 1;
+                } else if (++count == k) {
+                    sb.delete(i - k + 1, i + 1);
+                    break;
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    public String removeDuplicates1(String s, int k) {
+        StringBuilder sb = new StringBuilder(s);
+        int count[] = new int[sb.length()];
+        for (int i = 0; i < sb.length(); ++i) {
+            if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
+                count[i] = 1;
+            } else {
+                count[i] = count[i - 1] + 1;
+                if (count[i] == k) {
+                    sb.delete(i - k + 1, i + 1);
+                    i = i - k;
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    public String removeDuplicates2(String s, int k) {
+        StringBuilder sb = new StringBuilder(s);
+        Stack<Integer> counts = new Stack<>();
+        for (int i = 0; i < sb.length(); ++i) {
+            if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
+                counts.push(1);
+            } else {
+                int incremented = counts.pop() + 1;
+                if (incremented == k) {
+                    sb.delete(i - k + 1, i + 1);
+                    i = i - k;
+                } else {
+                    counts.push(incremented);
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    class Pair {
+        int cnt;
+        char ch;
+        public Pair(int cnt, char ch) {
+            this.ch = ch;
+            this.cnt = cnt;
+        }
+    }
+    public String removeDuplicates3(String s, int k) {
+        Stack<Pair> counts = new Stack<>();
+        for (int i = 0; i < s.length(); ++i) {
+            if (counts.empty() || s.charAt(i) != counts.peek().ch) {
+                counts.push(new Pair(1, s.charAt(i)));
+            } else {
+                if (++counts.peek().cnt == k) {
+                    counts.pop();
+                }
+            }
+        }
+        StringBuilder b = new StringBuilder();
+        while (!counts.empty()) {
+            Pair p = counts.pop();
+            for (int i = 0; i < p.cnt; i++) {
+                b.append(p.ch);
+            }
+        }
+        return b.reverse().toString();
+    }
+
+    public String removeDuplicates4(String s, int k) {
+        Stack<Integer> counts = new Stack<>();
+        char[] sa = s.toCharArray();
+        int j = 0;
+        for (int i = 0; i < s.length(); ++i, ++j) {
+            sa[j] = sa[i];
+            if (j == 0 || sa[j] != sa[j - 1]) {
+                counts.push(1);
+            } else {
+                int incremented = counts.pop() + 1;
+                if (incremented == k) {
+                    j = j - k;
+                } else {
+                    counts.push(incremented);
+                }
+            }
+        }
+        return new String(sa, 0, j);
+    }
+}
