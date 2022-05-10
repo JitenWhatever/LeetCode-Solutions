0cd3f3af58cd9455a3c9a66be527d0835b462590
Jitendra Kumar <jitendra1551k@gmail.com>
2022-05-10 20:11:03 +0530

    backtracking

--- Medium/CombinationSumIII.java
+++ Medium/CombinationSumIII.java
@@ -1,11 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
 Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
 
 Only numbers 1 through 9 are used.
 Each number is used at most once.
-Return a list of all possible valid combinations. The list must not contain the same combination twice, and the combinations may be returned in any order.
-
- 
+Return a list of all possible valid combinations. 
+The list must not contain the same combination twice, and the combinations may be returned in any order.
 
 Example 1:
 
@@ -28,7 +30,8 @@ Example 3:
 Input: k = 4, n = 1
 Output: []
 Explanation: There are no valid combinations.
-Using 4 different numbers in the range [1,9], the smallest sum we can get is 1+2+3+4 = 10 and since 10 > 1, there are no valid combination.
+Using 4 different numbers in the range [1,9], the smallest sum we can get is 1+2+3+4 = 10 and since 10 > 1, 
+there are no valid combination.
 Example 4:
 
 Input: k = 3, n = 2
@@ -49,7 +52,7 @@ Constraints:
 1 <= n <= 60
 */
 
-class Solution {
+public class CombinationSumIII  {
     public List<List<Integer>> combinationSum3(int k, int n) {
         
         result = new ArrayList<>();
@@ -67,7 +70,7 @@ class Solution {
         
         if(target == 0 && current.size() == k){ // k == 0
             
-            result.add(new ArrayList(current));
+            result.add(new ArrayList<>(current));
             return ;
         }
         
