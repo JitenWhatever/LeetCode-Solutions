17ca2e47e840dad0e3a5c6dc4b1b564db9b9724b
Jitendra Kumar <jitendra1551k@gmail.com>
2022-05-26 21:23:29 +0530

    bits

--- Easy/NumberOf1Bits.java
+++ Easy/NumberOf1Bits.java
@@ -1,5 +1,6 @@
 /*
-Write a function that takes an unsigned integer and return the number of '1' bits it has (also known as the Hamming weight).
+Write a function that takes an unsigned integer and return the number of '1' bits it has 
+(also known as the Hamming weight).
 
 Example 1:
 Input: 00000000000000000000000000001011
@@ -17,13 +18,16 @@ Output: 31
 Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
 
 Note:
-    Note that in some languages such as Java, there is no unsigned integer type. In this case, the input will be given as signed integer type and should not affect your implementation, as the internal binary representation of the integer is the same whether it is signed or unsigned.
-    In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3 above the input represents the signed integer -3.
+    Note that in some languages such as Java, there is no unsigned integer type. 
+    In this case, the input will be given as signed integer type and should not affect your implementation,
+     as the internal binary representation of the integer is the same whether it is signed or unsigned.
+    In Java, the compiler represents the signed integers using 2's complement notation. 
+    Therefore, in Example 3 above the input represents the signed integer -3.
 Follow up:
 If this function is called many times, how would you optimize it?
 */
 
-public class Solution {
+public class NumberOf1Bits {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         int bitCount = 0;
